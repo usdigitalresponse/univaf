4d122d2043362ff3834a73dbd7062137a2216538
Calvin French-Owen <calvin@calv.info>
2021-03-25 21:57:27 -0700

    terraform: adding initial terraform (#4)
    * terraform: adding initial terraform
    
    Let's see if this works! Borrowed mostly from this blog post:
    
    https://medium.com/@bradford_hamilton/deploying-containers-on-amazons-ecs-using-fargate-and-terraform-part-2-2e6f6a3a957f
    
    Repo:
    
    https://github.com/bradford-hamilton/terraform-ecs-fargate/tree/master/terraform

---
+++ terraform/ecs.tf
@@ -0,0 +1,49 @@
+# ecs.tf
+
+resource "aws_ecs_cluster" "main" {
+  name = "cluster"
+}
+
+data "template_file" "api" {
+  template = file("./templates/task_def.json.tpl")
+
+  vars = {
+    api_image      = var.api_image
+    api_port       = var.api_port
+    fargate_cpu    = var.fargate_cpu
+    fargate_memory = var.fargate_memory
+    aws_region     = var.aws_region
+  }
+}
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "api-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions    = data.template_file.api.rendered
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "api"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = var.api_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.api.id
+    container_name   = "api"
+    container_port   = var.api_port
+  }
+
+  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+}
---
+++ terraform/templates/task_def.json.tpl
@@ -0,0 +1,23 @@
+[
+  {
+    "name": "appointment-api",
+    "image": "${api_image}",
+    "cpu": ${fargate_cpu},
+    "memory": ${fargate_memory},
+    "networkMode": "awsvpc",
+    "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "/ecs/appointment-api",
+          "awslogs-region": "${aws_region}",
+          "awslogs-stream-prefix": "ecs"
+        }
+    },
+    "portMappings": [
+      {
+        "containerPort": ${api_port},
+        "hostPort": ${api_port}
+      }
+    ]
+  }
+]
