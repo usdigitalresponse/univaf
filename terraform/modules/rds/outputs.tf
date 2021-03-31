600fec703a0f5e1eec19e316ffcf755a737d49f1
Calvin French-Owen <calvin@calv.info>
2021-03-31 10:51:38 -0700

    rds: adding host output

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -5,3 +5,7 @@ output "addr" {
 output "url" {
   value = "${aws_db_instance.main.engine}://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}/${aws_db_instance.main.name}"
 }
+
+output "host" {
+  value = aws_db_instance.main.endpoint
+}
