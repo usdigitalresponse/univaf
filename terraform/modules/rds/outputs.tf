190fdba3d9e874255024fc22a2d04cc3e58af8a4
Calvin French-Owen <calvin@calv.info>
2021-03-31 11:15:10 -0700

    terraform: remove sensitive passwords

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,11 +1,3 @@
-output "addr" {
-  value = "${aws_db_instance.main.engine}://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}"
-}
-
-output "url" {
-  value = "${aws_db_instance.main.engine}://${aws_db_instance.main.username}:${aws_db_instance.main.password}@${aws_db_instance.main.endpoint}/${aws_db_instance.main.name}"
-}
-
 output "host" {
   value = aws_db_instance.main.endpoint
 }
