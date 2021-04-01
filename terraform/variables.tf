788143350594d9a04abe9d37e85294f9c79786df
Calvin French-Owen <calvin@calv.info>
2021-03-31 20:59:29 -0700

    updating variabls

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,12 +39,12 @@ variable "health_check_path" {
 
 variable "cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024"
+  default     = 1024
 }
 
 variable "memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "2048"
+  default     = 2048
 }
 
 variable "db_password" {
