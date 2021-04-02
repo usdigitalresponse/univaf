f2fde569159f433eab1c260503559906b627e732
Calvin French-Owen <calvin@calv.info>
2021-04-01 18:44:10 -0700

    tf: update iam

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@
 data "aws_iam_policy_document" "ecs_task_execution_role" {
   version = "2012-10-17"
   statement {
-    sid     = ""
+    sid     = "1"
     effect  = "Allow"
     actions = ["sts:AssumeRole", "iam:PassRole"]
 
@@ -10,6 +10,15 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com", "events.amazonaws.com"]
     }
+  }
+
+  statement {
+    sid = "2"
+    effect = "Allow"
+    actions = ["ecs:RunTask"]
+    resources = [
+      "*" # todo lock this down
+    ]
   }
 }
 
@@ -20,6 +29,11 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 # ECS task execution role policy attachment
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
