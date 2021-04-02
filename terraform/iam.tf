2921a91398a891248eda006f53dd6ab9b8ec2e8c
Calvin French-Owen <calvin@calv.info>
2021-04-01 18:46:20 -0700

    tf: fixing

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -33,8 +33,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
