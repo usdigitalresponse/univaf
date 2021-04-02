782ee2346461e63f4785a96df9459e8222cca5a0
Calvin French-Owen <calvin@calv.info>
2021-04-01 18:27:09 -0700

    iam: updating principal

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+      identifiers = ["ecs-tasks.amazonaws.com", "events.amazonaws.com"]
     }
   }
 }
