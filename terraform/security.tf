71625159d85580231bfa6861f09b97302a67324a
Calvin French-Owen <calvin@calv.info>
2021-04-07 10:46:58 -0700

    security groups: add port 443 (#37)
    This adds 443 to the public LB

--- terraform/security.tf
+++ terraform/security.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
