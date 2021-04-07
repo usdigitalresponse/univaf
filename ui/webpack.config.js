ef00fff2748cde50f1c1bbbd2c8a56fc2c42551e
Rob Brackett <rob@robbrackett.com>
2021-04-07 01:13:07 -0700

    Add demo UI (#33)
    The UI lives in the `ui` folder. You can start a process build & watch the source with `npm start` or just build with `npm run build`. The main idea here is that all the source is bundled into a single JS file so it's easy to drop directly onto a web page (no need for an iframe, for example). We should probably do some more to isolate CSS selectors, though.

---
+++ ui/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.jsx",
+  output: {
+    filename: "appointment-finder.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
+      DATA_URL: "",
+    }),
+    new HtmlWebpackPlugin({ inject: "body" }),
+  ],
+};
