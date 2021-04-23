748426b069eff43e899880db451031bb9b8a69c8
cavaccineinv <77516119+cavaccineinv@users.noreply.github.com>
2021-04-23 13:15:09 -0700

    Update loader/src/server.js
    Co-authored-by: Rob Brackett <rob@robbrackett.com>

--- loader/src/server.js
+++ loader/src/server.js
@@ -6,10 +6,16 @@ let port = process.env.PORT || 3000;
 function runServer(runFunc, options) {
   const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/plain");
-    runFunc(options).then((success) => {
-      res.statusCode = success ? 200 : 500;
-      res.end("Success: " + success);
-    });
+    runFunc(options)
+      .catch((error) => {
+        // Log the error internally. Should probably also trigger Sentry here.
+        console.error(error);
+        return false;
+      })
+      .then((success) => {
+        res.statusCode = success ? 200 : 500;
+        res.end("Success: " + success);
+      });
   });
 
   server.listen(port, hostname, () => {
