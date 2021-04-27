736ffea2b1f52e79c599eed2c6f5942c172d1df6
Aston Motes <astonm@gmail.com>
2021-04-26 17:49:20 -0700

    Update files for prettier

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,15 +1,15 @@
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       database: process.env.DB_NAME,
       user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
-      tableName: 'migrations'
-    }
-  }
+      tableName: "migrations",
+    },
+  },
 };
--- server/migrations/20210426212257_availability_valid_at.js
+++ server/migrations/20210426212257_availability_valid_at.js
@@ -1,12 +1,11 @@
-
-exports.up = function(knex) {
-  return knex.schema.alterTable('availability', t => {
-    t.renameColumn('updated_at', 'valid_at');
-  })
+exports.up = function (knex) {
+  return knex.schema.alterTable("availability", (t) => {
+    t.renameColumn("updated_at", "valid_at");
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema.alterTable('availability', t => {
-    t.renameColumn('valid_at', 'updated_at');
-  })
+exports.down = function (knex) {
+  return knex.schema.alterTable("availability", (t) => {
+    t.renameColumn("valid_at", "updated_at");
+  });
 };
