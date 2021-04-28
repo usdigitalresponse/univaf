5414b5d124be66ff700a419632797f07f0f58369
Rob Brackett <rob@robbrackett.com>
2021-04-27 19:05:14 -0700

    Productionize `valid_at` migration (#78)
    Get the `valid_at` migration from #75 ready for production. This cleans up a few minor issues:
    
    1. Production configuration for Knex.
    
    2. Actually run the migration in production! We now migrate at container startup. The actual implementation here may need some cleanup in a future PR.
    
    3. Allow `updated_at` in data sent to the `/update` endpoint from existing loaders that haven’t yet been updated, and transparently convert it to `valid_at`.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,15 +1,18 @@
-module.exports = {
-  development: {
-    client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: "migrations",
-    },
+const base = {
+  client: "postgresql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
   },
+  migrations: {
+    tableName: "migrations",
+  },
+};
+
+module.exports = {
+  development: base,
+  production: base,
 };
