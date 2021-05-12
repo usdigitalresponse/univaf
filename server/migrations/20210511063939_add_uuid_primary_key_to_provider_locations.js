d9714ce975d86ac1bbd19e8d6e873ab10a293786
Rob Brackett <rob@robbrackett.com>
2021-05-12 12:06:41 -0700

    Explain SQL file situation in comments

--- server/migrations/20210511063939_add_uuid_primary_key_to_provider_locations.js
+++ server/migrations/20210511063939_add_uuid_primary_key_to_provider_locations.js
@@ -8,6 +8,9 @@
  * up between systems, we should be using `location.external_ids`, rather than
  * hoping the `id` is set to something useful for the comparison needed.
  */
+
+// This migration is pretty messy to do via Knex abstractions, so the code is
+// in separate SQL files.
 const sql = require("./lib/sql");
 exports.up = sql.fileMigration(
   "20210511063939_add_uuid_primary_key_to_provider_locations.up.sql"
