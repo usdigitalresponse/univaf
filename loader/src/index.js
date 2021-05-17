4750a1e491aa2acd6814579c9d9fe905586da571
Rob Brackett <rob@robbrackett.com>
2021-05-17 13:05:33 -0700

    Create loader for CVS SMART Scheduling Links API
    CVS's new SMART Scheduling Links API includes slightly more detailed data than their previous API, and is open access (yay!). We can use it to cover the whole country for CVS.
    
    Fixes #77.

--- loader/src/index.js
+++ loader/src/index.js
@@ -1,6 +1,7 @@
 const sources = {
   cvsApi: require("./sources/cvs/api"),
   cvsScraper: require("./sources/cvs/scraper"),
+  cvsSmart: require("./sources/cvs/smart"),
   njvss: require("./sources/njvss"),
   riteAidApi: require("./sources/riteaid/api"),
   vaccinespotter: require("./sources/vaccinespotter"),
