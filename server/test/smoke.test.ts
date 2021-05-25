e49126021ee82ed562c6703ff4144f2ebd24797c
Rob Brackett <rob@robbrackett.com>
2021-05-25 13:30:37 -0700

    Add basic OpenAPI docs at `/docs` (#156)
    This also makes the root home page redirect to `/docs`. We'll probably want to flesh out the OpenAPI docs and add more descriptive information, but this is at least a start.
    
    The final piece that fixes #90.

--- server/test/smoke.test.ts
+++ server/test/smoke.test.ts
@@ -1,10 +1,13 @@
+import { URL } from "url";
 import app from "../src/app";
 import { useServerForTests } from "./lib";
 
 describe("GET /", function () {
   const context = useServerForTests(app);
-  it("should return 200 OK", async () => {
+
+  it("should redirect to docs index page", async () => {
     const res = await context.client.extend({ responseType: "text" }).get("");
+    expect(new URL(res.url)).toHaveProperty("pathname", "/docs/");
     expect(res.statusCode).toBe(200);
   });
 });
