850940b96eb8f45389f73b74bfa4b51011849964
Aston Motes <astonm@gmail.com>
2021-06-07 11:06:06 -0700

    Allow public access to all files in the S3 data snapshot bucket (#186)
    Fixes #185

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,17 @@ resource "aws_s3_bucket" "data_snapshots" {
   acl    = "public-read"
 }
 
+resource "aws_s3_bucket_policy" "data_snapshots" {
+  bucket = aws_s3_bucket.data_snapshots.id
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Id      = "Policy8542383977173"
+    Statement = [{
+      Sid       = "PublicReadAccess"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "arn:aws:s3:::univaf-data-snapshots/*"
+    }]
+  })
+}
