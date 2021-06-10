910a76dadc02c403804b00661d08347c82f5b75b
Rob Brackett <rob@robbrackett.com>
2021-06-10 07:57:16 -0700

    Add `merge-locations` script
    This script can be used to manually merge several specified locations together. It supports situations where the `remove-duplicate-locations` script cannot automatically identify duplicates that should be merged because they do not share external IDs -- for example, an NJVSS location and VaccineSpotter location that are the same, but the NJVSS record only has an NJVSS ID, while the VaccineSpotter one only has other IDs.
    
    List the IDs of locations to merge as arguments, where the first ID is the one others will be merged into:
    
        $ scripts/merge-locations.js <id_1> <id_2> <id_3> ...etc...
    
    Like the `remove-duplicate-locations` script, this will print a plan, but not actually merge unless you pass `--commit` as an argument.
    
    This also rewrites `remove-duplicate-locations` so that these two scripts can share much of their code, and should hopefully make that script easier to follow.

---
+++ server/scripts/merge-locations.js
@@ -0,0 +1,285 @@
+#!/usr/bin/env node
+/**
+ * Script for merging multiple locations together in the database.
+ *
+ * Call it with the IDs of the locations to be merged. The first ID is the
+ * location that others will be merged into:
+ *
+ *     $ scripts/merge-locations.js <id_1> <id_2> <id_3> ...etc...
+ *
+ * By default, this prints a plan of the steps it's going to take, but does not
+ * alter the contents of the database. Add the `--commit` option to actually
+ * make the changes.
+ *
+ * At the end of the run, a JSON object will be printed on STDOUT that maps
+ * old to new IDs for records that were merged.
+ *
+ * Merging means one location will be given all the external IDs and
+ * availability records of the others, and the others will be deleted. The IDs
+ * of the old locations are preserved as a `univaf_v1` external ID on the
+ * remaining location. If more than one is merged, their external IDs will have
+ * an incrementing number on the end, e.g. `univaf_v1`, `univaf_v1_2`,
+ * `univaf_v1_3`.
+ *
+ * Old, `univaf_v0` IDs are preserved on the remaining, merged location in the
+ * same way as described above for v1 IDs.
+ */
+
+const Knex = require("knex");
+const util = require("util");
+
+const NON_MERGE_FIELDS = new Set([
+  "id",
+  "external_ids",
+  "id_old",
+  "created_at",
+  "updated_at",
+]);
+
+function writeLog(...args) {
+  console.warn(...args);
+}
+
+const environment = process.env.NODE_ENV || "development";
+const db = Knex(require("../knexfile")[environment]);
+
+function locationsQuery() {
+  return db
+    .select(
+      "provider_locations.*",
+      db.raw(`
+        jsonb_agg(jsonb_build_object(
+          'system', external_ids.system,
+          'value', external_ids.value
+        )) as external_ids
+      `)
+    )
+    .from("provider_locations")
+    .leftOuterJoin(
+      "external_ids",
+      "provider_locations.id",
+      "external_ids.provider_location_id"
+    )
+    .groupBy("provider_locations.id");
+}
+
+async function loadLocation(id) {
+  return await locationsQuery().where("provider_locations.id", "=", id).first();
+}
+
+function planMerge(target, ...toMerge) {
+  // Determine what fields to update
+  let hasChanges = false;
+  let newData = {};
+
+  for (const source of toMerge) {
+    for (const key in source) {
+      if (NON_MERGE_FIELDS.has(key)) continue;
+
+      if (key === "meta") {
+        const meta = {
+          ...source.meta,
+          ...newData.meta,
+          ...target.meta,
+        };
+        if (!util.isDeepStrictEqual(meta || {}, target.meta || {})) {
+          newData.meta = meta;
+          hasChanges = true;
+        }
+      } else if (
+        newData[key] == null &&
+        target[key] == null &&
+        source[key] != null
+      ) {
+        newData[key] = source[key];
+        hasChanges = true;
+      }
+    }
+  }
+
+  // Determine new set of external IDs
+  const allIds = [target, ...toMerge].flatMap((x) => [
+    { system: "univaf_v1", value: x.id },
+    ...x.external_ids,
+  ]);
+  const newIds = dedupeNewExternalIds(allIds, target.id);
+
+  return {
+    targetId: target.id,
+    newIds,
+    newData: hasChanges ? newData : undefined,
+    deleteLocations: toMerge.map((x) => x.id),
+  };
+}
+
+function dedupeNewExternalIds(allExternalIds, newId) {
+  const v0Ids = new Set();
+  const v1Ids = new Set();
+
+  // Extract v0 and v1 IDs and dedupe their values.
+  allExternalIds = allExternalIds.filter((id) => {
+    if (id.system.startsWith("univaf_v0")) {
+      v0Ids.add(id.value);
+      return false;
+    } else if (id.system.startsWith("univaf_v1")) {
+      v1Ids.add(id.value);
+      return false;
+    }
+    return true;
+  });
+
+  // Don't keep a reference to ourselves in the external IDs!
+  v1Ids.delete(newId);
+
+  // Add the v0/v1 IDs back it an integer on the system name to allow for more
+  // than one as v0/v1.
+  let index = 0;
+  v0Ids.forEach((value) => {
+    index++;
+    let system = "univaf_v0";
+    if (index > 1) system = `${system}_${index}`;
+
+    allExternalIds.push({ system, value });
+  });
+  index = 0;
+  v1Ids.forEach((value) => {
+    index++;
+    let system = "univaf_v1";
+    if (index > 1) system = `${system}_${index}`;
+
+    allExternalIds.push({ system, value });
+  });
+
+  // Remove duplicates.
+  const seen = new Map();
+  return allExternalIds.filter((id) => {
+    // Remove duplicates and sanity check.
+    const existing = seen.get(id.system);
+    if (existing) {
+      if (existing !== id.value) {
+        // Special case a known, OK scenario because of bad data in NJVSS
+        const badPair = ["10907:WALMART_2582", "10882:WALMART_2582"];
+        if (badPair.includes(existing) && badPair.includes(id.value)) {
+          // Just filter one out.
+          return false;
+        }
+
+        // Bail out if we need to set the same system to multiple values.
+        throw new Error(
+          `Multiple values for system "${id.system}": ("${id.value}", "${existing}")`
+        );
+      } else {
+        // Drop it if it's just a duplicate.
+        return false;
+      }
+    } else {
+      seen.set(id.system, id.value);
+      return true;
+    }
+  });
+}
+
+async function doMerge(plan, persist = false) {
+  writeLog("Merging into", plan.targetId);
+
+  for (const from of plan.deleteLocations) writeLog("  from", from);
+
+  for (const newId of plan.newIds) {
+    writeLog("  Adding ID:", JSON.stringify(newId));
+  }
+  if (plan.newData) {
+    writeLog("  Updating fields:", JSON.stringify(plan.newData));
+  }
+
+  if (persist) {
+    await db.transaction(async (trx) => {
+      // Add new external IDs
+      if (plan.newIds) {
+        await trx("external_ids")
+          .insert(
+            plan.newIds.map((newId) => ({
+              ...newId,
+              provider_location_id: plan.targetId,
+            }))
+          )
+          .onConflict(["provider_location_id", "system"])
+          .merge();
+      }
+
+      // Merge main fields of provider_locations
+      await trx("provider_locations")
+        .update({
+          ...plan.newData,
+          updated_at: new Date(),
+        })
+        .where("id", plan.targetId);
+
+      for (const deleteId of plan.deleteLocations) {
+        // Copy newer availability entries
+        const availabilities = await trx("availability").where({
+          location_id: deleteId,
+        });
+        for (const entry of availabilities) {
+          delete entry.id;
+          await trx("availability")
+            .insert({
+              ...entry,
+              location_id: plan.targetId,
+            })
+            .where("availability.checked_at", "<", entry.checked_at)
+            .onConflict(["location_id", "source"])
+            .merge();
+        }
+
+        // Delete from availability
+        await trx("availability").where({ location_id: deleteId }).delete();
+        // Delete from external_ids
+        await trx("external_ids")
+          .where({ provider_location_id: deleteId })
+          .delete();
+        // Delete from provider_locations
+        await trx("provider_locations").where({ id: deleteId }).delete();
+      }
+    });
+  }
+}
+
+async function main(args) {
+  const ids = args.filter((arg) => !arg.startsWith("--"));
+  const options = args.filter((arg) => arg.startsWith("--"));
+  const commit = options.includes("--commit");
+
+  if (ids.length < 2) {
+    writeLog("Please specify at least two locations to merge.");
+    process.exitCode = 1;
+    return;
+  }
+
+  const locations = await Promise.all(ids.map((id) => loadLocation(id)));
+  // console.log(locations[0]);
+  const plan = planMerge(...locations);
+  await doMerge(plan, commit);
+
+  if (!commit) {
+    writeLog("");
+    writeLog("This is a plan. Run with --commit to actually make changes.");
+  }
+}
+
+module.exports = {
+  NON_MERGE_FIELDS,
+  db,
+  locationsQuery,
+  dedupeNewExternalIds,
+  planMerge,
+  doMerge,
+};
+
+if (require.main === module) {
+  main(process.argv.slice(2))
+    .catch((error) => {
+      console.error(error);
+      process.exitCode = 1;
+    })
+    .finally(() => db.destroy());
+}
--- server/scripts/remove-duplicate-locations.js
+++ server/scripts/remove-duplicate-locations.js
@@ -21,28 +21,7 @@
  * same way as described above for v1 IDs.
  */
 
-const Knex = require("knex");
-const util = require("util");
-
-const db = Knex({
-  client: "postgresql",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-  },
-  migrations: {
-    tableName: "migrations",
-  },
-  pool: {
-    afterCreate(conn, done) {
-      // Ensure Postgres formats times in UTC.
-      conn.query('SET timezone="UTC";', done);
-    },
-  },
-});
+const { db, planMerge, doMerge, locationsQuery } = require("./merge-locations");
 
 function writeLog(...args) {
   console.warn(...args);
@@ -53,26 +32,12 @@ function writeData(text) {
 }
 
 async function loadLocations() {
-  const locations = await db
-    .select(
-      "provider_locations.*",
-      db.raw(
-        "jsonb_object_agg(external_ids.system, external_ids.value) as external_ids"
-      )
-    )
-    .from("provider_locations")
-    .leftOuterJoin(
-      "external_ids",
-      "provider_locations.id",
-      "external_ids.provider_location_id"
-    )
-    .groupBy("provider_locations.id");
-
+  const locations = await locationsQuery();
   writeLog("Total locations:", locations.length);
   return locations;
 }
 
-function findDuplicates(locations) {
+function groupLocations(locations) {
   const byId = new Map();
   const byExternalId = new Map();
 
@@ -80,7 +45,7 @@ function findDuplicates(locations) {
   for (const location of locations) {
     byId.set(location.id, location);
 
-    for (let [system, id] of Object.entries(location.external_ids)) {
+    for (let { system, value } of location.external_ids) {
       // Skip not-quite-unique systems
       if (system === "vtrcks") continue;
       // Skip internal identifiers
@@ -91,10 +56,10 @@ function findDuplicates(locations) {
       // so this is safe.
       if (system === "storeNumber") {
         system = "cvs";
-        id = id.toString().padStart(5, "0");
+        value = value.toString().padStart(5, "0");
       }
 
-      const simpleId = `${system}:${id}`;
+      const simpleId = `${system}:${value}`;
       let locationSet = byExternalId.get(simpleId);
       if (!locationSet) {
         locationSet = [];
@@ -108,106 +73,46 @@ function findDuplicates(locations) {
 }
 
 /**
- * @param {Map} locations
- * @param {Map} byExternalId
+ * @param {Map<string,any>} locations
+ * @param {Map<string,Array>} byExternalId
  * @param {boolean} persist
  */
-function planChanges(locations, byExternalId) {
-  const toMerge = new Map();
-  const toUpdate = new Map();
+function planChanges(byId, byExternalId) {
+  // Union all the intersecting sets of locations with matching external IDs
+  // so we have a list of all groups of locations that have some external IDs
+  // in common.
+  const groupMap = new Map();
+  for (const idGroup of byExternalId.values()) {
+    if (idGroup.length === 1) continue;
 
-  for (const locations of byExternalId.values()) {
-    // If there was only one locations for this external ID, we're good.
-    if (locations.length === 1) continue;
+    let parentSets = idGroup.map((x) => groupMap.get(x)).filter(Boolean);
+    let homeSet = parentSets.shift() || new Set();
+    for (const parent of parentSets) {
+      if (parent === homeSet) continue;
 
-    // Superset of all external IDs we want to store for the merged location.
-    let allIds = [];
-    for (const location of locations) {
-      for (const [system, value] of Object.entries(location.external_ids)) {
-        // These are bad IDs that should not be kept.
-        if (system === "storeNumber") continue;
-
-        allIds.push({ system, value });
-      }
-      // Keep track of the current ID for when things merge, too.
-      allIds.push({ system: "univaf_v1", value: location.id });
-    }
-
-    const removed = [];
-    let remaining = [];
-    // Make sure we drop the one that has `storeNumber` if it's in the set.
-    for (const location of locations) {
-      if (location.external_ids.storeNumber) {
-        removed.push(location);
-      } else {
-        remaining.push(location);
+      for (const element of parent) {
+        homeSet.add(element);
+        groupMap.set(element, homeSet);
       }
     }
-
-    // If one has already been marked for keeping, pull that out.
-    let kept;
-    for (let i = 0; i < remaining.length; i++) {
-      const item = remaining[i];
-      if (toUpdate.has(item.id)) {
-        kept = item;
-        allIds = allIds.concat(toUpdate.get(item.id).newIds);
-        remaining.splice(i, 1);
-        break;
-      }
+    for (const location of idGroup) {
+      homeSet.add(location);
+      groupMap.set(location, homeSet);
     }
-    // Otherwise take the earliest created one.
-    if (!kept) {
-      remaining.sort((a, b) => a.created_at - b.created_at);
-      kept = remaining.shift();
-    }
+  }
 
-    // Mark the rest for removal.
-    for (const location of remaining) {
-      // Sanity-check it's not marked to be kept elsewhere
-      if (toUpdate.has(location.id)) {
-        allIds = allIds.concat(toUpdate.get(location.id).newIds);
-        toUpdate.delete(location.id);
-        // Update any that were merging into this to now target what this is
-        // merging into.
-        for (const [mergeFrom, mergeTo] of [...toMerge.entries()]) {
-          if (mergeTo === location.id) {
-            toMerge.set(mergeFrom, kept.id);
-            break;
-          }
-        }
-      }
-
-      removed.push(location);
-    }
-
-    for (const removable of removed) {
-      const alreadyPlanned = toMerge.get(removable.id);
-      // Handle situation where the location to be merged is already set to
-      // merge into a different location.
-      if (alreadyPlanned && alreadyPlanned !== kept.id) {
-        // Find any others targeted at that location and re-target them here.
-        for (const [mergeFrom, mergeTo] of [...toMerge.entries()]) {
-          if (mergeTo === alreadyPlanned) toMerge.set(mergeFrom, kept.id);
-        }
-        // Add all the old merge target's IDs to our full ID set
-        allIds = allIds.concat(toUpdate.get(alreadyPlanned).newIds);
-        // Change the old merge target to merge into the new one here.
-        toUpdate.delete(alreadyPlanned);
-        toMerge.set(alreadyPlanned, kept.id);
-      }
-      toMerge.set(removable.id, kept.id);
-    }
-
-    toUpdate.set(kept.id, {
-      location: kept,
-      newIds: dedupeNewExternalIds(allIds, kept.id),
-      newData: null,
-    });
+  // Plan merges for each group.
+  const plans = [];
+  for (const idGroup of new Set(groupMap.values())) {
+    // Sort by oldest first, so that the oldest becomes the target all the
+    // others merge into.
+    const sorted = [...idGroup].sort((a, b) => a.created_at - b.created_at);
+    plans.push(planMerge(...sorted));
   }
 
   // Sanity check all the external ID pairs are unique
   const seenPairs = new Set();
-  for (const { newIds } of toUpdate.values()) {
+  for (const { newIds } of plans) {
     for (const id of newIds) {
       const pair = `${id.system}:${id.value}`;
       if (seenPairs.has(pair)) {
@@ -220,46 +125,7 @@ function planChanges(locations, byExternalId) {
     }
   }
 
-  // Merge other data fields.
-  const doNotMerge = new Set([
-    "id",
-    "external_ids",
-    "id_old",
-    "created_at",
-    "updated_at",
-  ]);
-  for (const [removableId, targetId] of toMerge.entries()) {
-    const removable = locations.get(removableId);
-    const target = toUpdate.get(targetId);
-    let hasChanges = false;
-    let newData = target.newData || {};
-
-    for (const key in removable) {
-      if (key === "meta") {
-        newData.meta = {
-          ...removable.meta,
-          ...newData.meta,
-          ...target.location.meta,
-        };
-        hasChanges = !util.isDeepStrictEqual(
-          newData.meta,
-          target.location.meta
-        );
-      } else if (
-        !doNotMerge.has(key) &&
-        newData[key] == null &&
-        target.location[key] == null &&
-        removable[key] != null
-      ) {
-        newData[key] = removable[key];
-        hasChanges = true;
-      }
-    }
-
-    if (hasChanges) target.newData = newData;
-  }
-
-  return { toUpdate, toMerge };
+  return plans;
 }
 
 /**
@@ -267,187 +133,40 @@ function planChanges(locations, byExternalId) {
  * @param {Map} toRemove
  * @param {boolean} persist
  */
-async function doChanges(toUpdate, toMerge, persist = false) {
-  const reverseMergeMap = new Map();
-  for (const [mergeFrom, mergeTo] of toMerge.entries()) {
-    let froms = reverseMergeMap.get(mergeTo);
-    if (!froms) {
-      froms = [];
-      reverseMergeMap.set(mergeTo, froms);
-    }
-    froms.push(mergeFrom);
+async function doChanges(plans, persist = false) {
+  let removed = 0;
+  let updated = 0;
+  for (const plan of plans) {
+    updated++;
+    removed += plan.deleteLocations.length;
+    doMerge(plan, persist);
   }
 
-  const remainingUpdates = new Map(toUpdate);
-  for (const [mergeTo, mergeFroms] of reverseMergeMap.entries()) {
-    writeLog("Merging into", mergeTo);
-    for (const from of mergeFroms) writeLog("  from", from);
-    const updates = remainingUpdates.get(mergeTo);
-    if (updates) {
-      remainingUpdates.delete(mergeTo);
-      for (const newId of updates.newIds) {
-        writeLog("  Adding ID:", JSON.stringify(newId));
-      }
-      if (updates.newData) {
-        writeLog("  Updating fields:", JSON.stringify(updates.newData));
-      }
-    }
-
-    if (persist) {
-      await db.transaction(async (trx) => {
-        // Add new external IDs
-        if (updates) {
-          await trx("external_ids")
-            .insert(
-              updates.newIds.map((newId) => ({
-                ...newId,
-                provider_location_id: mergeTo,
-              }))
-            )
-            .onConflict(["provider_location_id", "system"])
-            .merge();
-        }
-
-        // Merge main fields of provider_locations
-        await trx("provider_locations")
-          .update({
-            ...updates.newData,
-            updated_at: new Date(),
-          })
-          .where("id", mergeTo);
-
-        for (const from of mergeFroms) {
-          // Copy newer availability entries
-          const availabilities = await trx("availability").where({
-            location_id: from,
-          });
-          for (const entry of availabilities) {
-            delete entry.id;
-            await trx("availability")
-              .insert({
-                ...entry,
-                location_id: mergeTo,
-              })
-              .where("availability.checked_at", "<", entry.checked_at)
-              .onConflict(["location_id", "source"])
-              .merge();
-          }
-
-          // Delete from availability
-          await trx("availability").where({ location_id: from }).delete();
-          // Delete from external_ids
-          await trx("external_ids")
-            .where({ provider_location_id: from })
-            .delete();
-          // Delete from provider_locations
-          await trx("provider_locations").where({ id: from }).delete();
-        }
-      });
-    }
-  }
-
-  for (const [id, updates] of remainingUpdates.entries()) {
-    writeLog("Updating", id);
-    for (const newId of updates.newIds) {
-      writeLog("  Adding ID:", JSON.stringify(newId));
-    }
-    if (persist) {
-      await db("external_ids")
-        .insert(
-          updates.newIds.map((newId) => ({
-            ...newId,
-            provider_location_id: id,
-          }))
-        )
-        .onConflict(["provider_location_id", "system"])
-        .merge();
-    }
-  }
-
-  writeLog(`Removing ${toMerge.size} duplicates`);
-  writeLog(`Adding IDs to ${toUpdate.size} locations`);
-}
-
-function dedupeNewExternalIds(allIds, newId) {
-  const v0Ids = new Set();
-  const v1Ids = new Set();
-
-  // Extract v0 and v1 IDs and dedupe their values.
-  allIds = allIds.filter((id) => {
-    if (id.system.startsWith("univaf_v0")) {
-      v0Ids.add(id.value);
-      return false;
-    } else if (id.system.startsWith("univaf_v1")) {
-      v1Ids.add(id.value);
-      return false;
-    }
-    return true;
-  });
-
-  // Don't keep a reference to ourselves in the external IDs!
-  v1Ids.delete(newId);
-
-  // Add the v0/v1 IDs back it an integer on the system name to allow for more
-  // than one as v0/v1.
-  let index = 0;
-  v0Ids.forEach((value) => {
-    index++;
-    let system = "univaf_v0";
-    if (index > 1) system = `${system}_${index}`;
-
-    allIds.push({ system, value });
-  });
-  index = 0;
-  v1Ids.forEach((value) => {
-    index++;
-    let system = "univaf_v1";
-    if (index > 1) system = `${system}_${index}`;
-
-    allIds.push({ system, value });
-  });
-
-  // Remove duplicates.
-  const seen = new Map();
-  return allIds.filter((id) => {
-    // Remove duplicates and sanity check.
-    const existing = seen.get(id.system);
-    if (existing) {
-      if (existing !== id.value) {
-        // Special case a known, OK scenario because of bad data in NJVSS
-        const badPair = ["10907:WALMART_2582", "10882:WALMART_2582"];
-        if (badPair.includes(existing) && badPair.includes(id.value)) {
-          // Just filter one out.
-          return false;
-        }
-
-        // Bail out if we need to set the same system to multiple values.
-        throw new Error(
-          `Multiple values for system "${id.system}": ("${id.value}", "${existing}")`
-        );
-      } else {
-        // Drop it if it's just a duplicate.
-        return false;
-      }
-    } else {
-      seen.set(id.system, id.value);
-      return true;
-    }
-  });
+  writeLog(`Removing ${removed} duplicates`);
+  writeLog(`Adding IDs to ${updated} locations`);
 }
 
 async function main() {
   const commit = process.argv.includes("--commit");
 
   const locations = await loadLocations();
-  const { byId, byExternalId } = findDuplicates(locations);
-  const plan = planChanges(byId, byExternalId);
+  const { byId, byExternalId } = groupLocations(locations);
+  const plans = planChanges(byId, byExternalId);
 
-  await doChanges(plan.toUpdate, plan.toMerge, commit);
+  await doChanges(plans, commit);
 
-  const mappingOutput = [...plan.toMerge.entries()].map(([from_id, to_id]) => {
-    return { from_id, to_id };
+  const mappingOutput = plans.flatMap(({ targetId, deleteLocations }) => {
+    return deleteLocations.map((deleteId) => ({
+      from_id: deleteId,
+      to_id: targetId,
+    }));
   });
   writeData(JSON.stringify(mappingOutput, null, 2));
+
+  if (!commit) {
+    writeLog("");
+    writeLog("This is a plan. Run with --commit to actually make changes.");
+  }
 }
 
 main()
