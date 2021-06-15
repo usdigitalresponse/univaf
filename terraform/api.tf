0a300ca19bf52079606ea35725ae6988bfd33db2
Aston Motes <astonm@gmail.com>
2021-06-14 20:37:08 -0700

    Add terraform config for Cloudfront distribution

--- terraform/api.tf
+++ terraform/api.tf
@@ -192,3 +192,48 @@ resource "aws_cloudwatch_log_stream" "api_log_stream" {
   log_group_name = aws_cloudwatch_log_group.api_log_group.name
 }
 
+
+# Add API server caching
+resource "aws_cloudfront_distribution" "univaf_api" {
+  enabled     = true
+  price_class = "PriceClass_100" # North America
+
+  origin {
+    origin_id   = var.domain_name
+    domain_name = var.domain_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = var.domain_name
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    max_ttl                = 3600
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
