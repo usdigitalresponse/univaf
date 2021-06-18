e65e611798f49173745cdcf1d35030580c2fddd2
Rob Brackett <rob@robbrackett.com>
2021-06-18 11:20:55 -0700

    Add failing tests for `?sources` query param

--- server/test/api.edge.test.ts
+++ server/test/api.edge.test.ts
@@ -107,6 +107,48 @@ describe("GET /api/edge/locations", () => {
       products: ["pfizer", "moderna"],
     });
   });
+
+  it("limits sources used when `?sources=x` is set", async () => {
+    const location = await createLocation(TestLocation);
+    await updateAvailability(location.id, {
+      source: "test-system-1",
+      checked_at: new Date(),
+      available: Availability.YES,
+      available_count: 5,
+    });
+    await updateAvailability(location.id, {
+      source: "test-system-2",
+      checked_at: new Date(),
+      available: Availability.YES,
+      products: ["pfizer", "moderna"],
+    });
+    await updateAvailability(location.id, {
+      source: "test-system-3",
+      checked_at: new Date(),
+      available: Availability.YES,
+      capacity: [
+        {
+          date: "2021-05-13",
+          available: Availability.YES,
+        },
+      ],
+    });
+
+    let res = await context.client.get<any>({
+      url: "api/edge/locations",
+      searchParams: { sources: "test-system-1,test-system-2" },
+    });
+    expect(res.body.data).toHaveLength(1);
+    expect(res.body.data[0].availability).toEqual({
+      sources: ["test-system-2", "test-system-1"],
+      checked_at: expectDatetimeString(),
+      valid_at: expectDatetimeString(),
+      changed_at: expectDatetimeString(),
+      available: Availability.YES,
+      available_count: 5,
+      products: ["pfizer", "moderna"],
+    });
+  });
 });
 
 describe("GET /api/edge/locations/:id", () => {
