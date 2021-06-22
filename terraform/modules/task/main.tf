a3d4d98906a7a872f20856c4d8c079c14fc4764a
Jacob Aronoff <jacobaronoff45@gmail.com>
2021-06-22 17:57:17 -0400

    A real noop

--- terraform/modules/task/main.tf
+++ terraform/modules/task/main.tf
@@ -79,5 +79,35 @@ resource "aws_ecs_task_definition" "main" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = var.cpu
   memory                   = var.memory
-  container_definitions    = jsonencode([local.container_definition])
+  container_definitions = jsonencode([
+    {
+      cpu    = var.cpu
+      memory = var.memory
+
+      name  = var.name
+      image = var.image
+
+      environment = [for key, val in var.env_vars : { name = key, value = val }]
+      entryPoint  = var.entry_point
+      command     = var.command
+
+      essential   = true
+      networkMode = "awsvpc"
+      mountPoints = []
+      portMappings = [
+        {
+          containerPort = var.port
+          hostPort      = var.port
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group" = "/ecs/${var.name}"
+          "awslogs-region"  = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
 }
