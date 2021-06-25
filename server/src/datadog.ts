5c64871f30565e89ef171163a5c07da2111fd1f1
Jacob Aronoff <jacobaronoff45@gmail.com>
2021-06-25 18:22:09 -0400

    Prettier

--- server/src/datadog.ts
+++ server/src/datadog.ts
@@ -3,7 +3,7 @@ import StatsD from "hot-shots";
 
 export const dogstatsd = new StatsD();
 const stat = "node.express.router";
-const DELIM = '-';
+const DELIM = "-";
 const REGEX_PIPE = /\|/g;
 
 export interface MonitoredRequest extends Request {
@@ -14,7 +14,7 @@ export interface MonitoredRequest extends Request {
  * Checks if str is a regular expression and stringifies it if it is.
  * Returns a string with all instances of the pipe character replaced with
  * the delimiter.
- * 
+ *
  * Taken from the original connect-datadog module
  * {@link https://github.com/DataDog/node-connect-datadog/blob/master/lib/index.js#L15-L28}
  */
@@ -27,7 +27,7 @@ function replacePipeChar(str: String): String {
 }
 
 function getRoute(req: MonitoredRequest): String {
-	const routePath = req.route && req.route.path ? req.route.path : '';
+	const routePath = req.route && req.route.path ? req.route.path : "";
 	return replacePipeChar(routePath);
 }
 
@@ -50,16 +50,11 @@ export function datadogMiddleware(
 		statTags.push(`method:${req.method.toLowerCase()}`);
 		statTags.push(`response_code:${res.statusCode}`);
 
-		const now = new Date()
+		const now = new Date();
 		const responseTime = now.valueOf() - req.startTime.valueOf();
 
 		dogstatsd.increment(`${stat}.response_total`, 1, statTags);
-		dogstatsd.histogram(
-			`${stat}.response_time`,
-			responseTime,
-			1,
-			statTags
-		);
+		dogstatsd.histogram(`${stat}.response_time`, responseTime, 1, statTags);
 	});
 	next();
 }
