ea656597de9b7c82d7b6e0287e3f466d27f38054
Rob Brackett <rob@robbrackett.com>
2021-06-25 16:02:04 -0700

    Add new `univaf-*` container registries (#258)
    This adds the new registries so we can start using them in #255. If we merge this first, that should resolve the build errors in that PR, and we won’t have build errors when we first land it.
    
    This will still need two independent follow-up steps:
    1. Change the CI workflow to push to the new names.
    2. Once we've pushed some images, change the API and loader tasks to pull from them.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -24,3 +24,30 @@ resource "aws_ecr_repository" "loader" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_repository" "server_repository" {
+  name                 = "univaf-server"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "seed_repository" {
+  name                 = "univaf-db-seed"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "loader_repository" {
+  name                 = "univaf-loader"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
