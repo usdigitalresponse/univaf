78467c3971dbc4db035aabac691372510513dc25
Jacob Aronoff <jacobaronoff45@gmail.com>
2021-06-29 10:53:07 -0400

    Remove sinon, update tests

--- server/test/datadog.test.ts
+++ server/test/datadog.test.ts
@@ -1,5 +1,4 @@
 import {
-  expectDatetimeString,
   installTestDatabaseHooks,
   useServerForTests,
 } from "./lib";
@@ -8,15 +7,15 @@ import { createLocation, getLocationById, updateAvailability } from "../src/db";
 import { TestLocation, TestLocation2 } from "./fixtures";
 import { datadogMiddleware, MonitoredRequest, dogstatsd } from "../src/datadog";
 import { Response } from "express";
-import sinon from "sinon";
+
 installTestDatabaseHooks();
 
 const mockResponse = () => {
-  const res = {} as Response;
-  res.status = sinon.stub().returns(res);
-  res.json = sinon.stub().returns(res);
-  res.on = jest.fn();
-  return res;
+  return ({
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+    on: jest.fn(),
+  } as any) as Response;
 };
 
 describe("datadog middleware", () => {
@@ -24,30 +23,18 @@ describe("datadog middleware", () => {
     dogstatsd.mockBuffer = [];
   });
 
-  describe("request handler creation", () => {
-    it("should return a function()", () => {
-      expect(typeof datadogMiddleware).toBe("function");
-    });
-
-    it("should accept three arguments", () => {
-      expect(datadogMiddleware.length).toEqual(3);
-    });
-  });
-
   describe("request handler calling", () => {
     const context = useServerForTests(app);
     it("should call next() once", () => {
-      var nextSpy = sinon.spy();
+      let nextSpy = jest.fn();
       const mockRequest = {
         body: {},
       } as MonitoredRequest;
       const res = mockResponse();
       datadogMiddleware(mockRequest, res, nextSpy);
-      expect(nextSpy.calledOnce).toEqual(true);
+      expect(nextSpy.mock.calls).toHaveLength(1);
     });
     it("should have correct metrics on single endpoint call", async () => {
-      const location = await createLocation(TestLocation);
-      await updateAvailability(location.id, TestLocation.availability);
       const res = await context.client.get<any>("api/edge/locations");
       expect(res.statusCode).toBe(200);
 
@@ -56,25 +43,23 @@ describe("datadog middleware", () => {
       expect(dogstatsd.mockBuffer).toContain(
         "node.express.router.response_total:1|c|#route:/api/edge/locations,method:get,response_code:200"
       );
-      expect(dogstatsd.mockBuffer[1]).toMatch(
-        /node.express.router.response_time.*/
+      expect(dogstatsd.mockBuffer).toContainEqual(
+        expect.stringMatching(/^node\.express\.router\.response_time:/)
       );
     });
     it("should have correct metrics on two endpoint calls", async () => {
-      const location = await createLocation(TestLocation);
-      await updateAvailability(location.id, TestLocation.availability);
-      var res = await context.client.get<any>(
-        `api/edge/locations/${location.id}`
-      );
-      expect(res.statusCode).toBe(200);
-      res = await context.client.get<any>(`api/edge/locations/${location.id}`);
-      expect(res.statusCode).toBe(200);
+      let res = await context.client.get<any>(`api/edge/locations/x`, {
+        retry: 0,
+      });
+      expect(res.statusCode).toBe(404);
+      res = await context.client.get<any>(`api/edge/locations/x`, { retry: 0 });
+      expect(res.statusCode).toBe(404);
 
       expect(
         dogstatsd.mockBuffer.filter(
           (s) =>
             s ===
-            "node.express.router.response_total:1|c|#route:/api/edge/locations/:id,method:get,response_code:200"
+            "node.express.router.response_total:1|c|#route:/api/edge/locations/:id,method:get,response_code:404"
         )
       ).toHaveLength(2);
     });
