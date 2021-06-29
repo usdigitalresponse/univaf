e53ebfc383a1ac28bc4b6d57c6e8aa0f935ede4e
Rob Brackett <rob@robbrackett.com>
2021-06-29 11:41:36 -0700

    Alphabetize rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -9,16 +9,16 @@ module.exports = {
         allowArgumentsExplicitlyTypedAsAny: true,
       },
     ],
-    // We have some plain JS in the codebase that we don't want a build step
-    // for; they need `require()` support.
-    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
         argsIgnorePattern: "^_",
       },
     ],
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    // We have some plain JS in the codebase that we don't want a build step
+    // for; they need `require()` support.
+    "@typescript-eslint/no-var-requires": "off",
   },
 };
