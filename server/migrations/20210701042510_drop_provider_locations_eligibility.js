7e258783c63ffd4628210f2339d6ef1cfb7226fa
Chantel Miller <cmiller20@gmail.com>
2021-07-01 01:21:56 -0400

    DOC

--- server/migrations/20210701042510_drop_provider_locations_eligibility.js
+++ server/migrations/20210701042510_drop_provider_locations_eligibility.js
@@ -1,3 +1,10 @@
+/*
+  Drops eligibility field on provider locations.
+  We originally thought we’d be able to gather eligibility requirements,
+  like minimum age, whether only same-county residents are allowed, etc.
+  in a structured way, but that hasn’t really borne out,
+  and where we have this kind of info, it’s part of the description.
+*/
 
 exports.up = function(knex) {
   return knex.schema.alterTable("provider_locations", (table) => {
