6ee8a81f5d66458de98207d5a4f801a838a91f44
Aston Motes <astonm@gmail.com>
2021-07-22 14:30:22 -0700

    Fixes for ESLint

--- server/migrations/20210722181933_unpadded_ext_ids.js
+++ server/migrations/20210722181933_unpadded_ext_ids.js
@@ -1,10 +1,10 @@
 exports.up = async function (knex) {
-  zeroPaddedRows = await knex("external_ids")
+  const zeroPaddedRows = await knex("external_ids")
     .select("provider_location_id", "system", "value")
     .where("system", "cvs")
     .where("value", "~", "^0\\d+$");
 
-  unpaddedRows = zeroPaddedRows.map((r) => ({
+  const unpaddedRows = zeroPaddedRows.map((r) => ({
     ...r,
     value: r.value.replace(/^0+/, ""),
   }));
@@ -12,6 +12,6 @@ exports.up = async function (knex) {
   return knex.batchInsert("external_ids", unpaddedRows);
 };
 
-exports.down = async function (knex) {
+exports.down = async function () {
   console.log("20210722181933_unpadded_ext_ids.js: No migrated rows removed.");
 };
