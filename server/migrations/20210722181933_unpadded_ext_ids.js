b0f52c6285e253f2dfafc922ab610f70a9572d9a
Aston Motes <astonm@gmail.com>
2021-07-26 12:40:36 -0700

    Create migration to add unpadded store ids for CVS locations (#309)

---
+++ server/migrations/20210722181933_unpadded_ext_ids.js
@@ -0,0 +1,22 @@
+exports.up = async function (knex) {
+  const zeroPaddedRows = await knex("external_ids")
+    .select("provider_location_id", "system", "value")
+    .where("system", "cvs")
+    .where("value", "~", String.raw`^0\d+$`);
+
+  const unpaddedRows = zeroPaddedRows.map((r) => ({
+    ...r,
+    value: r.value.replace(/^0+/, ""),
+  }));
+
+  if (unpaddedRows.length) {
+    return knex("external_ids")
+      .insert(unpaddedRows)
+      .onConflict(["provider_location_id", "system", "value"])
+      .ignore();
+  }
+};
+
+exports.down = async function () {
+  console.log("20210722181933_unpadded_ext_ids.js: No migrated rows removed.");
+};
