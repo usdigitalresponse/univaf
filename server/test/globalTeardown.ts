2a664a501a70ac6c202256bb6ecac92f00e9928c
Rob Brackett <rob@robbrackett.com>
2021-08-12 20:32:56 -0700

    Log whether sentry actually closed

--- server/test/globalTeardown.ts
+++ server/test/globalTeardown.ts
@@ -2,6 +2,6 @@ import * as Sentry from "@sentry/node";
 
 module.exports = async () => {
   console.log("HELLO FROM GLOBAL TEARDOWN");
-  await Sentry.close();
-  console.log("DONE WITH GLOBAL TEARDOWN");
+  const result = await Sentry.close();
+  console.log(`DONE WITH GLOBAL TEARDOWN. Sentry closed? ${result}`);
 };
