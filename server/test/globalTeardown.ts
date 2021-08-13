2354db14ccd0445adb76bdc1c8d853fe8846c7b5
Rob Brackett <rob@robbrackett.com>
2021-08-12 20:14:08 -0700

    Make sure teardown is running

--- server/test/globalTeardown.ts
+++ server/test/globalTeardown.ts
@@ -1,5 +1,7 @@
 import * as Sentry from "@sentry/node";
 
 module.exports = async () => {
+  console.log("HELLO FROM GLOBAL TEARDOWN");
   await Sentry.close();
+  console.log("DONE WITH GLOBAL TEARDOWN");
 };
