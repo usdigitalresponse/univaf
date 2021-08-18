usdigitalresponse/univaf

commit 991a5931e9c30754879f0b6f1c0ae9abb2e1af02

Move `external_ids` to new table (#147)
* Move provider_location.external_ids to external table

* Add a test for updating position

* Switch to knex query building for update

* Support external_id-based getById for edge API

* Move clearTestDatabase() to jest globalSetup

Fixes #103

server/src/db.ts
@@ -19,38 +19,8 @@ const DEFAULT_BATCH_SIZE = 2000;
 
 export const db = Knex(loadDbConfig());
 
-export function assertIsTestDatabase() {
-  return db.raw("SELECT current_database() as name;").then((result) => {
-    const databaseName: string = result.rows[0].name;
-    if (!databaseName.endsWith("-test")) {
-      throw new Error(
-        `Expected to be connected to the test database. Currently connected to ${databaseName}!`
-      );
-    }
-  });
-}
-
-export async function clearTestDatabase() {
-  await assertIsTestDatabase();
-
-  const res = await db.raw(
-    "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname='public'"
-  );
-
-  await Promise.all(
-    res.rows.map(async (row: any) => {
-      if (row.tablename != "spatial_ref_sys") {
-        return db.raw(`DROP TABLE ${row.tablename} CASCADE`);
-      }
-    })
-  );
-
-  await db.migrate.latest();
-}
-
 const providerLocationFields = [
   "id",
-  "external_ids",
   "provider",
   "location_type",
   "name",
@@ -123,46 +93,70 @@ export async function createLocation(data: any): Promise<ProviderLocation> {
     RETURNING id`,
     sqlFields.map((x) => x[1] || null)
   );
-  return await getLocationById(inserted.rows[0].id);
+
+  const locationId = inserted.rows[0].id;
+  await setExternalIds(locationId, data.external_ids);
+  return await getLocationById(locationId);
+}
+
+/**
+ * Set external ids for a provider location.
+ * Note that this will remove any existing external ids for the location.
+ * @param id Provider location ID
+ * @param externalIds {system: value}
+ */
+export async function setExternalIds(
+  id: string,
+  externalIds: { string: string }
+): Promise<void> {
+  await db("external_ids")
+    .insert(
+      Object.entries(externalIds).map(([system, value]: [string, string]) => {
+        return {
+          provider_location_id: id,
+          system,
+          value,
+        };
+      })
+    )
+    .onConflict(["provider_location_id", "system"])
+    .merge();
 }
 
 /**
  * Update data about a provider location.
+ * @param location ProviderLocation
  * @param data ProviderLocation-like object with data to update
  */
 export async function updateLocation(
+  location: ProviderLocation,
   data: any,
   { mergeSubfields = true } = {}
-): Promise<ProviderLocation> {
-  const id = data.id;
-  if (!data.id) throw new Error("Location must have an ID to update");
+): Promise<void> {
+  const sqlData: any = { updated_at: new Date() };
 
-  const now = new Date();
-  const sqlData = {
-    ...data,
-    updated_at: now,
-  };
-  if ("position" in sqlData) {
-    sqlData.position = formatSqlPoint(sqlData.position);
+  for (let [key, value] of Object.entries(data)) {
+    if (key == "position") {
+      value = formatSqlPoint(data.position);
+    }
+
+    if (key == "meta" && mergeSubfields) {
+      value = db.raw('"meta" || ?', JSON.stringify(data.meta));
+    }
+
+    if (key != "id" && providerLocationAllFields.includes(key)) {
+      sqlData[key] = value;
+    }
   }
 
-  const sqlFields = Object.entries(sqlData).filter(([key, _]) => {
-    return providerLocationAllFields.includes(key);
-  });
-  const setExpression = sqlFields.map(([key, _]) => {
-    // Merge external_ids and meta, rather than replacing.
-    if (mergeSubfields && (key === "external_ids" || key === "meta")) {
-      return `${key} = ${key} || ?`;
-    }
-    return `${key} = ?`;
-  });
-  const result = await db.raw(
-    `UPDATE provider_locations
-    SET ${setExpression}
-    WHERE id = ?`,
-    [...sqlFields.map((x) => x[1]), id]
-  );
-  return result.rows[0];
+  await db("provider_locations").where("id", location.id).update(sqlData);
+
+  if ("external_ids" in data) {
+    await setExternalIds(location.id, {
+      ...location.external_ids,
+      ...data.external_ids,
+    });
+  }
 }
 
 /**
@@ -219,6 +213,9 @@ export async function listLocations({
     throw error;
   }
 
+  const locationIds = result.rows.map((r: any) => r.id);
+  const externalIds = await getExternalIdsByLocation(locationIds);
+
   return result.rows.map((row: any) => {
     // The SELECT expression always creates an object; not sure if there's a
     // good way to get it to output `NULL` instead for this case.
@@ -230,6 +227,8 @@ export async function listLocations({
       delete row.availability.is_public;
       delete row.availability.rank;
     }
+
+    row.external_ids = externalIds[row.id] || {};
 
     return row;
   });
@@ -332,6 +331,8 @@ export async function getLocationById(
   id: string,
   { includePrivate = false } = {}
 ): Promise<ProviderLocation | undefined> {
+  if (!UUID_PATTERN.test(id)) return;
+
   const rows = await listLocations({
     includePrivate,
     limit: 1,
@@ -348,8 +349,8 @@ export async function getLocationById(
  * @returns ProviderLocation | undefined
  */
 export async function getLocationByExternalIds(
-  externalIds: { string: string },
-  { includePrivate = false } = {}
+  externalIds: { [k: string]: string },
+  { includePrivate = false, includeExternalIds = false } = {}
 ): Promise<ProviderLocation | undefined> {
   // Some IDs are not unique enough to identify a single location (e.g.
   // VTrckS PINs), so remove them from the set of external IDs to query.
@@ -368,23 +369,68 @@ export async function getLocationByExternalIds(
     fields = fields.concat(providerLocationPrivateFields);
   }
 
-  return await db("provider_locations")
+  const location = await db("provider_locations")
+    .join(
+      "external_ids",
+      "external_ids.provider_location_id",
+      "provider_locations.id"
+    )
     .select(
-      fields.map((name) =>
+      fields.map((name) => {
+        name = `provider_locations.${name}`;
         // Ensure we return geo coordinates in an easy-to-handle format.
-        name === "position" ? db.raw(selectSqlPoint(name)) : name
-      )
+        return name === "position" ? db.raw(selectSqlPoint(name)) : name;
+      })
     )
     .modify((builder) => {
       if (!includePrivate) builder.where("is_public", true);
     })
     .andWhere((builder) => {
       for (const [system, id] of queryableIds) {
-        // @ts-expect-error: Knex's typings aren't quite right here :(
-        builder.orWhere("external_ids", "@>", { [system]: id });
+        builder.orWhere((builder) => {
+          builder
+            .where("external_ids.system", "=", system)
+            .andWhere("external_ids.value", "=", id);
+        });
       }
     })
     .first();
+
+  if (location && includeExternalIds) {
+    const externalIds = await getExternalIdsByLocation(location.id);
+    location.external_ids = externalIds[location.id];
+  }
+
+  return location;
+}
+
+interface ExternalIdsByLocation {
+  [locationId: string]: { [system: string]: string };
+}
+/**
+ * Returns a mapping of provider location id to external ids
+ * @param locationIds (string | string[])
+ * @returns ExternalIdsByLocation
+ */
+export async function getExternalIdsByLocation(
+  locationIds: string | string[]
+): Promise<ExternalIdsByLocation> {
+  const selectIds = Array.isArray(locationIds) ? locationIds : [locationIds];
+
+  const rows = await db("external_ids")
+    .select("provider_location_id", "system", "value")
+    .whereIn("provider_location_id", selectIds);
+
+  const out: ExternalIdsByLocation = {};
+  for (const row of rows) {
+    if (!(row.provider_location_id in out)) {
+      out[row.provider_location_id] = {};
+    }
+
+    out[row.provider_location_id][row.system] = row.value;
+  }
+
+  return out;
 }
 
 /**

server/jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
   testMatch: ["**/test/**/*.test.(ts|js)"],
   testEnvironment: "node",
   coverageReporters: ["text", "html"],
+  globalSetup: "<rootDir>/test/globalSetup.ts",
 };

server/migrations/20210520181353_create_external_ids_table.js
@@ -1,0 +1,46 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("external_ids", (t) => {
+    t.increments("id");
+    t.uuid("provider_location_id");
+    t.text("system");
+    t.text("value");
+    t.timestamps(true, true);
+
+    t.unique(["provider_location_id", "system"]);
+    t.index(["system", "value"]);
+    t.foreign("provider_location_id").references("provider_locations.id");
+  });
+
+  await migrateExternalIds(knex);
+
+  await knex.schema.table("provider_locations", (t) => {
+    t.renameColumn("external_ids", "external_ids_old");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.table("provider_locations", (t) => {
+    t.renameColumn("external_ids_old", "external_ids");
+  });
+
+  await knex.schema.dropTable("external_ids");
+};
+
+async function migrateExternalIds(knex) {
+  const locations = await knex("provider_locations").select(
+    "id",
+    "external_ids"
+  );
+
+  const rows = [];
+  for (const location of locations) {
+    for (const [system, value] of Object.entries(location.external_ids)) {
+      rows.push({
+        provider_location_id: location.id,
+        system,
+        value,
+      });
+    }
+  }
+  return knex.batchInsert("external_ids", rows);
+}

server/src/api/edge.ts
@@ -9,6 +9,8 @@ import { Pagination, UUID_PATTERN } from "../utils";
 /** Maximum time for streaming lists to run for in seconds. */
 const MAX_STREAMING_TIME = 25 * 1000;
 
+const EXTERNAL_ID_PATTERN = /^(?<system>[^:]+):(?<value>.+)$/;
+
 /**
  * Send an error response.
  * @param response HTTP Response to write to
@@ -146,11 +148,23 @@ export const getById = async (req: AppRequest, res: Response) => {
     return sendError(res, "Not authorized for private data", 403);
   }
 
-  const provider = await db.getLocationById(id, { includePrivate });
+  let provider: any = null;
+  const m = id.match(EXTERNAL_ID_PATTERN);
+  if (m) {
+    provider = await db.getLocationByExternalIds(
+      { [m.groups.system]: m.groups.value },
+      { includePrivate, includeExternalIds: true }
+    );
+  }
+
+  if (!provider && UUID_PATTERN.test(id)) {
+    provider = await db.getLocationById(id, { includePrivate });
+  }
+
   if (!provider) {
     return sendError(res, `No provider location with ID '${id}'`, 404);
   } else {
-    res.json({ data: provider });
+    return res.json({ data: provider });
   }
 };
 
@@ -208,8 +222,7 @@ export const update = async (req: AppRequest, res: Response) => {
     // need to opt in to updating here.)
     const fields = Object.keys(data).filter((key) => key !== "availability");
     if (fields.length > 1) {
-      data.id = location.id;
-      await db.updateLocation(data);
+      await db.updateLocation(location, data);
       result.location.action = "updated";
     }
   }

server/src/interfaces.ts
@@ -42,7 +42,7 @@ export interface SlotRecord {
 
 export interface ProviderLocation {
   id: string;
-  external_ids: object;
+  external_ids: { [system: string]: string };
   provider: string;
   location_type: LocationType;
   name: string;

server/test/api.edge.test.ts
@@ -2,7 +2,7 @@ import { useServerForTests, installTestDatabaseHooks } from "./lib";
 import { getApiKeys } from "../src/config";
 import app from "../src/app";
 import { createLocation, getLocationById, updateAvailability } from "../src/db";
-import { TestLocation } from "./fixtures";
+import { TestLocation, TestLocation2 } from "./fixtures";
 import { Availability } from "../src/interfaces";
 
 installTestDatabaseHooks();
@@ -10,12 +10,17 @@ installTestDatabaseHooks();
 describe("GET /api/edge/locations", () => {
   const context = useServerForTests(app);
 
-  it("responds with a list of locations", async () => {
+  it("responds with a list of locations containing external_ids", async () => {
     const location = await createLocation(TestLocation);
     await updateAvailability(location.id, TestLocation.availability);
     const res = await context.client.get<any>("api/edge/locations");
     expect(res.statusCode).toBe(200);
     expect(res.body.data).toHaveLength(1);
+
+    expect(res.body.data[0]).toHaveProperty(
+      "external_ids",
+      TestLocation.external_ids
+    );
   });
 
   it("responds with a list of locations filtered by state", async () => {
@@ -50,7 +55,7 @@ describe("GET /api/edge/locations", () => {
 describe("GET /api/edge/locations/:id", () => {
   const context = useServerForTests(app);
 
-  it("responds with location status", async () => {
+  it("responds with location status containing external_ids", async () => {
     const location = await createLocation(TestLocation);
     await updateAvailability(location.id, TestLocation.availability);
 
@@ -64,6 +69,83 @@ describe("GET /api/edge/locations/:id", () => {
       "data.location_type",
       TestLocation.location_type
     );
+    expect(res.body).toHaveProperty(
+      "data.external_ids",
+      TestLocation.external_ids
+    );
+  });
+
+  it("can be found by external_id containing external_ids", async () => {
+    const location = await createLocation(TestLocation);
+    await updateAvailability(location.id, TestLocation.availability);
+
+    const externalId = Object.entries(TestLocation.external_ids)[0];
+
+    const res = await context.client.get<any>(
+      `api/edge/locations/${externalId[0]}:${externalId[1]}`
+    );
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("data.id", location.id);
+    expect(res.body).toHaveProperty("data.name", TestLocation.name);
+    expect(res.body).toHaveProperty(
+      "data.location_type",
+      TestLocation.location_type
+    );
+    expect(res.body).toHaveProperty(
+      "data.external_ids",
+      TestLocation.external_ids
+    );
+  });
+
+  it("does not mistakenly select by external_id", async () => {
+    const location = await createLocation(TestLocation);
+    await updateAvailability(location.id, TestLocation.availability);
+
+    const externalId = Object.entries(TestLocation.external_ids)[0];
+
+    let res = await context.client.get<any>(
+      `api/edge/locations/thisthing:doesntexist`
+    );
+    expect(res.statusCode).toBe(404);
+
+    res = await context.client.get<any>(
+      `api/edge/locations/${externalId[0]}:doesntexist`
+    );
+    expect(res.statusCode).toBe(404);
+
+    res = await context.client.get<any>(
+      `api/edge/locations/${externalId[0]}:${externalId[1]}`
+    );
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("data.id", location.id);
+    expect(res.body).toHaveProperty(
+      "data.external_ids",
+      TestLocation.external_ids
+    );
+  });
+
+  it("responds correctly with multiple locations", async () => {
+    const location1 = await createLocation(TestLocation);
+    let res = await context.client.get<any>("api/edge/locations");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.data).toHaveLength(1);
+
+    const location2 = await createLocation(TestLocation2);
+    res = await context.client.get<any>("api/edge/locations");
+    expect(res.statusCode).toBe(200);
+    expect(res.body.data).toHaveLength(2);
+
+    res = await context.client.get<any>(
+      `api/edge/locations/njiis:${TestLocation.external_ids.njiis}`
+    );
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("data.id", location1.id);
+
+    res = await context.client.get<any>(
+      `api/edge/locations/njiis:${TestLocation2.external_ids.njiis}`
+    );
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("data.id", location2.id);
   });
 });
 
@@ -281,5 +363,24 @@ describe("POST /api/edge/update", () => {
       throwHttpErrors: false,
     });
     expect(response.statusCode).toBe(422);
+  });
+
+  it("should update position", async () => {
+    const location = await createLocation(TestLocation);
+    const newPosition = { longitude: 30, latitude: 26 };
+
+    let res = await context.client.post("api/edge/update?update_location=1", {
+      headers,
+      json: {
+        id: location.id,
+        position: newPosition,
+      },
+    });
+    expect(res.statusCode).toBe(200);
+
+    res = await context.client.get(`api/edge/locations/${location.id}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("data.id", location.id);
+    expect(res.body).toHaveProperty("data.position", newPosition);
   });
 });

server/test/fixtures.ts
@@ -39,3 +39,35 @@ export const TestLocation = {
     meta: {},
   },
 };
+
+export const TestLocation2 = {
+  id: "0cd44819-4761-45f3-8544-cf1793310d31",
+  external_ids: {
+    njiis: "nj5678",
+    vtrcks: "789",
+    rite_aid: "576",
+    univaf_v0: "rite_aid:576",
+  },
+  provider: "RiteAid",
+  location_type: "PHARMACY",
+  name: "Rite Aid #576",
+  address_lines: ["605 North Colony Road"],
+  city: "Wallingford",
+  state: "CT",
+  postal_code: "06492-3109",
+  info_phone: "(203) 265-3600",
+  info_url: "https://www.riteaid.com/covid-19",
+  booking_phone: "(203) 265-3600",
+  booking_url: "https://www.riteaid.com/pharmacy/covid-qualifier",
+  requires_waitlist: false,
+  created_at: "2021-04-27T20:20:32.498Z",
+  updated_at: "2021-04-27T20:20:32.498Z",
+  availability: {
+    source: "rite-aid-api",
+    valid_at: "2021-05-15T02:31:44+00:00",
+    checked_at: "2021-05-15T02:32:14.449+00:00",
+    available: "YES",
+    meta: {},
+    available_count: 191,
+  },
+};

server/test/globalSetup.ts
@@ -1,0 +1,38 @@
+import { knex } from "knex";
+import { loadDbConfig } from "../src/config";
+
+export const testDb = knex(loadDbConfig());
+
+module.exports = async () => {
+  await clearTestDatabase();
+  await testDb.destroy();
+};
+
+export function assertIsTestDatabase() {
+  return testDb.raw("SELECT current_database() as name;").then((result) => {
+    const databaseName: string = result.rows[0].name;
+    if (!databaseName.endsWith("-test")) {
+      throw new Error(
+        `Expected to be connected to the test database. Currently connected to ${databaseName}!`
+      );
+    }
+  });
+}
+
+export async function clearTestDatabase() {
+  await assertIsTestDatabase();
+
+  const res = await testDb.raw(
+    "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname='public'"
+  );
+
+  await Promise.all(
+    res.rows.map(async (row: any) => {
+      if (row.tablename != "spatial_ref_sys") {
+        return testDb.raw(`DROP TABLE ${row.tablename} CASCADE`);
+      }
+    })
+  );
+
+  await testDb.migrate.latest();
+}

commit ed5c8c9004d1be98c8cd4ffe37bcf947f74625ff

Fix position format in getLocationByExternalIds
It appears we broke this in #147. The practical impact is that `/api/edge/locations/<external_id>` returned positions in WKB format instead of an object like `{latitude: 1.23, longitude: 1.23}`.

server/src/db.ts
@@ -397,7 +397,9 @@ export async function getLocationByExternalIds(
       fields.map((name) => {
         name = `provider_locations.${name}`;
         // Ensure we return geo coordinates in an easy-to-handle format.
-        return name === "position" ? db.raw(selectSqlPoint(name)) : name;
+        return name === "provider_locations.position"
+          ? db.raw(selectSqlPoint(name))
+          : name;
       })
     )
     .modify((builder) => {