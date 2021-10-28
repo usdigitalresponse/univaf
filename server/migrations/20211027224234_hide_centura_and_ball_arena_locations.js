6b4592d540a1a8066fbc1822e69ca9bc430f8be3
Rob Brackett <rob@robbrackett.com>
2021-10-27 17:46:09 -0700

    Add migration to hide mass vax sites in CO (#423)

---
+++ server/migrations/20211027224234_hide_centura_and_ball_arena_locations.js
@@ -0,0 +1,19 @@
+/**
+ * The Centura Driveup Event and Denver Ball Arena locations in Colorado were
+ * mass vaccination sites tht have since closed. Make them private so they
+ * don't show up in the API anymore.
+ */
+
+exports.up = async function (knex) {
+  const result = await knex("provider_locations")
+    .where("provider", ["centura_driveup_event", "denver_ball_arena"])
+    .update({ is_public: false });
+
+  console.log("Updated", result, "locations");
+};
+
+exports.down = async function () {
+  console.log(
+    "hide_centura_and_ball_arena_locations.js: No migrated rows changed."
+  );
+};
