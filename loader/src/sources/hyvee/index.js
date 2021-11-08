5e62c2e309a1840f66d05283f594ecd9f320f30c
Rob Brackett <rob@robbrackett.com>
2021-11-07 23:55:58 -0800

    Finish out basic HyVee formatting

--- loader/src/sources/hyvee/index.js
+++ loader/src/sources/hyvee/index.js
@@ -1,10 +1,21 @@
 const Sentry = require("@sentry/node");
-const { httpClient } = require("../../utils");
-const { LocationType, VaccineProduct, Available } = require("../../model");
-const { ParseError } = require("../../exceptions");
+const { httpClient, matchVaccineProduct } = require("../../utils");
+const { LocationType, Available } = require("../../model");
 
 const API_URL = "https://www.hy-vee.com/my-pharmacy/api/graphql";
 const BOOKING_URL = "https://www.hy-vee.com/my-pharmacy/covid-vaccine-consent";
+const SOURCE_NAME = "univaf-hyvee";
+const PROVIDER_NAME = "hyvee";
+
+function warn(message, context) {
+  console.warn(`HyVee: ${message}`, context);
+  // Sentry does better fingerprinting with an actual exception object.
+  if (message instanceof Error) {
+    Sentry.captureException(message, { level: Sentry.Severity.Info });
+  } else {
+    Sentry.captureMessage(message, Sentry.Severity.Info);
+  }
+}
 
 async function fetchRawData() {
   const response = await httpClient(API_URL, {
@@ -64,24 +75,30 @@ async function fetchRawData() {
 }
 
 function formatLocation(data, checkedAt) {
-  // implement me!
-
-  // covidVaccineEligibilityTerms usually is 'No eligibility terms defined.'
-  // Manufacturers: Set(4) { 'Pfizer-BioNTech', 'Flu Vaccine', 'Moderna', 'Janssen' }
-
   const address_lines = [data.address.line1];
   if (data.address.line2) {
     address_lines.push(data.address.line2);
   }
 
+  // covidVaccineEligibilityTerms may sometimes have useful info, but is often
+  // 'No eligibility terms defined.'
+  let description;
+  if (
+    data.covidVaccineEligibilityTerms &&
+    !/no eligibility terms/i.test(data.covidVaccineEligibilityTerms)
+  ) {
+    description = data.covidVaccineEligibilityTerms;
+  }
+
   return {
     // Store names in this data don't clarify that we are talking about HyVee.
+    // e.g. "Waterloo #2" is the name of the second store in Waterloo, IA.
     name: `HyVee ${data.name}`,
     external_ids: [
       ["hyvee", data.locationId],
       ["hyvee_store", data.businessCode.toString()],
     ],
-    provider: "hyvee",
+    provider: PROVIDER_NAME,
     location_type: LocationType.pharmacy,
     address_lines,
     city: data.address.city,
@@ -94,20 +111,33 @@ function formatLocation(data, checkedAt) {
 
     info_phone: data.phoneNumber,
     booking_url: BOOKING_URL,
+    description,
     meta: { hyvee_nickname: data.nickname },
-    is_public: true,
 
     availability: {
-      source: "univaf-hyvee",
+      source: SOURCE_NAME,
       checked_at: checkedAt,
-      is_public: true,
       available: data.covidVaccineTimeSlotAndManufacturerAvailability
         .isCovidVaccineAvailable
         ? Available.yes
         : Available.no,
-      products: ["NOPE"],
+      products: formatProducts(
+        data.covidVaccineTimeSlotAndManufacturerAvailability
+      ),
     },
   };
+}
+
+function formatProducts(vaccineData) {
+  return vaccineData.availableCovidVaccineManufacturers
+    .filter((product) => product.manufacturerName !== "Flu Vaccine")
+    .map((product) => {
+      const result = matchVaccineProduct(product.manufacturerName);
+      if (!result) {
+        warn(`Unknown product type "${product.manufacturerName}"`);
+      }
+      return result;
+    });
 }
 
 module.exports = {
--- loader/test/hyvee.test.js
+++ loader/test/hyvee.test.js
@@ -1,69 +1,73 @@
-const { formatLocation } = require("../src/sources/hyvee");
+const { checkAvailability, formatLocation } = require("../src/sources/hyvee");
 const { Available, LocationType } = require("../src/model");
 const { expectDatetimeString } = require("./support");
+const { locationSchema } = require("./support/schemas");
+const stateData = require("../src/states.json");
+
+const RawLocation = {
+  locationId: "fa22572e-ad2b-4cec-a787-dec0fb3ea086",
+  name: "Waterloo #2",
+  nickname: "Logan",
+  phoneNumber: "+13192348627",
+  businessCode: "1863",
+  covidVaccineTimeSlotAndManufacturerAvailability: {
+    isCovidVaccineAvailable: true,
+    availableCovidVaccineManufacturers: [
+      {
+        covidVaccineManufacturerId: "e7388e53-33dd-48a2-bf9c-10a883a4c98c",
+        manufacturerName: "Moderna",
+        isSingleDose: false,
+        isBooster: false,
+        __typename: "CovidVaccineManufacturer",
+      },
+      {
+        covidVaccineManufacturerId: "2616ff22-429e-4e48-83ae-be3835fcc2aa",
+        manufacturerName: "Pfizer-BioNTech",
+        isSingleDose: false,
+        isBooster: false,
+        __typename: "CovidVaccineManufacturer",
+      },
+      {
+        covidVaccineManufacturerId: "189d79f5-22d3-4e8a-856d-b7dd2139eb39",
+        manufacturerName: "Flu Vaccine",
+        isSingleDose: true,
+        isBooster: false,
+        __typename: "CovidVaccineManufacturer",
+      },
+      {
+        covidVaccineManufacturerId: "d827e77f-b89c-47fa-9678-77929c5eae54",
+        manufacturerName: "Janssen",
+        isSingleDose: true,
+        isBooster: false,
+        __typename: "CovidVaccineManufacturer",
+      },
+      {
+        covidVaccineManufacturerId: "dd8e390a-10c9-466f-85b9-18b8348a813d",
+        manufacturerName: "Pediatric-Pfizer",
+        isSingleDose: false,
+        isBooster: false,
+        __typename: "CovidVaccineManufacturer",
+      },
+    ],
+    __typename: "CovidVaccineTimeSlotAndManufacturerAvailability",
+  },
+  covidVaccineEligibilityTerms: "No eligibility terms defined.",
+  address: {
+    line1: "2181 Logan Ave",
+    line2: null,
+    city: "Waterloo",
+    state: "IA",
+    zip: "50703",
+    latitude: 42.53037,
+    longitude: -92.33859,
+    __typename: "LocationAddress",
+  },
+  __typename: "Location",
+};
 
 describe("H-E-B", () => {
   it("should format correct output for a store", () => {
-    const formatted = formatLocation(
-      {
-        locationId: "fa22572e-ad2b-4cec-a787-dec0fb3ea086",
-        name: "Waterloo #2",
-        nickname: "Logan",
-        phoneNumber: "+13192348627",
-        businessCode: "1863",
-        covidVaccineTimeSlotAndManufacturerAvailability: {
-          isCovidVaccineAvailable: true,
-          availableCovidVaccineManufacturers: [
-            {
-              covidVaccineManufacturerId:
-                "e7388e53-33dd-48a2-bf9c-10a883a4c98c",
-              manufacturerName: "Moderna",
-              isSingleDose: false,
-              isBooster: false,
-              __typename: "CovidVaccineManufacturer",
-            },
-            {
-              covidVaccineManufacturerId:
-                "2616ff22-429e-4e48-83ae-be3835fcc2aa",
-              manufacturerName: "Pfizer-BioNTech",
-              isSingleDose: false,
-              isBooster: false,
-              __typename: "CovidVaccineManufacturer",
-            },
-            {
-              covidVaccineManufacturerId:
-                "189d79f5-22d3-4e8a-856d-b7dd2139eb39",
-              manufacturerName: "Flu Vaccine",
-              isSingleDose: true,
-              isBooster: false,
-              __typename: "CovidVaccineManufacturer",
-            },
-            {
-              covidVaccineManufacturerId:
-                "d827e77f-b89c-47fa-9678-77929c5eae54",
-              manufacturerName: "Janssen",
-              isSingleDose: true,
-              isBooster: false,
-              __typename: "CovidVaccineManufacturer",
-            },
-          ],
-          __typename: "CovidVaccineTimeSlotAndManufacturerAvailability",
-        },
-        covidVaccineEligibilityTerms: "No eligibility terms defined.",
-        address: {
-          line1: "2181 Logan Ave",
-          line2: null,
-          city: "Waterloo",
-          state: "IA",
-          zip: "50703",
-          latitude: 42.53037,
-          longitude: -92.33859,
-          __typename: "LocationAddress",
-        },
-        __typename: "Location",
-      },
-      "2021-10-11T00:00:00Z"
-    );
+    const formatted = formatLocation(RawLocation, "2021-10-11T00:00:00Z");
 
     expect(formatted).toEqual({
       name: "HyVee Waterloo #2",
@@ -83,14 +87,50 @@ describe("H-E-B", () => {
       },
       info_phone: "+13192348627",
       booking_url: "https://www.hy-vee.com/my-pharmacy/covid-vaccine-consent",
-      is_public: true,
       meta: { hyvee_nickname: "Logan" },
       availability: {
         source: "univaf-hyvee",
         available: Available.yes,
         checked_at: expectDatetimeString(),
-        products: ["jj", "moderna", "pfizer"],
+        products: ["moderna", "pfizer", "jj", "pfizer_age_5_11"],
       },
     });
+    expect(formatted).toMatchSchema(locationSchema);
+  });
+
+  it("handles multi-line addresses", () => {
+    const formatted = formatLocation(
+      {
+        ...RawLocation,
+        address: {
+          ...RawLocation.address,
+          line1: "first line",
+          line2: "second line",
+        },
+      },
+      "2021-10-11T00:00:00Z"
+    );
+
+    expect(formatted).toHaveProperty("address_lines", [
+      "first line",
+      "second line",
+    ]);
+  });
+
+  it("adds eligibility info to the description", () => {
+    const formatted = formatLocation({
+      ...RawLocation,
+      covidVaccineEligibilityTerms: "Must be taller than 4 feet for this ride",
+    });
+    expect(formatted).toHaveProperty(
+      "description",
+      "Must be taller than 4 feet for this ride"
+    );
+  });
+
+  it("should output valid data", async () => {
+    const states = stateData.map((x) => x.usps);
+    const result = await checkAvailability(() => {}, { states });
+    expect(result).toContainItemsMatchingSchema(locationSchema);
   });
 });
