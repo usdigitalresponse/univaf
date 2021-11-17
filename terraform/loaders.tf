3ae6ebf5b7709cf08f1f8c5fc122ffadb3177b63
Rob Brackett <rob@robbrackett.com>
2021-11-16 16:47:33 -0800

    Deploy H-E-B loader (#465)
    Deploy @cmroberts’s loader from #452 (and add @cmroberts as a contributor)!

--- terraform/loaders.tf
+++ terraform/loaders.tf
@@ -117,6 +117,21 @@ module "hyvee_loader" {
   subnets       = aws_subnet.public.*.id
 }
 
+module "heb_loader" {
+  source = "./modules/loader"
+
+  name          = "heb"
+  command       = ["--states", "AL,AK,AZ,AR,CA,CO,CT,DE,DC,FL,GA,HI,ID,IL,IN,IA,KS,KY,LA,ME,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,OH,OK,OR,PA,RI,SC,SD,TN,TX,UT,VT,VA,WA,WV,WI,WY,MH,PR,VI"]
+  loader_source = "heb"
+  api_url       = "http://${aws_alb.main.dns_name}"
+  api_key       = var.api_key
+  sentry_dsn    = var.loader_sentry_dsn
+  schedule      = "cron(8/10 * * * ? *)"
+  cluster_arn   = aws_ecs_cluster.main.arn
+  role          = aws_iam_role.ecs_task_execution_role.arn
+  subnets       = aws_subnet.public.*.id
+}
+
 module "washington_doh_loader" {
   source = "./modules/loader"
 
