60f4609488ae7bf2fcd188d9ed37ffe9b0591ea2
Rob Brackett <rob@robbrackett.com>
2022-02-16 15:10:16 -0800

    Set up Terraform dependencies correctly (#570)
    This is a follow-on to #569. It turns out we were using an old method of configuring Terraform dependencies and versions that is deprecated. This updates it and adds Dependabot for for Terraform and actions (but not for NPM, where we are still using Snyk for now).

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = var.aws_region
-  version = "3.74.2"
 }
