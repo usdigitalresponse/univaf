a65f0010c0195b0ce76aa37f6c70dcab3fd1823e
Rob Brackett <rob@robbrackett.com>
2022-04-17 16:46:23 -0700

    Make Docker image tags immutable in ECR
    Fixes #364.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -30,7 +30,7 @@ resource "aws_ecr_repository" "loader" {
 
 resource "aws_ecr_repository" "server_repository" {
   name                 = "univaf-server"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
@@ -39,7 +39,7 @@ resource "aws_ecr_repository" "server_repository" {
 
 resource "aws_ecr_repository" "seed_repository" {
   name                 = "univaf-db-seed"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
@@ -48,7 +48,7 @@ resource "aws_ecr_repository" "seed_repository" {
 
 resource "aws_ecr_repository" "loader_repository" {
   name                 = "univaf-loader"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
