55f1987ba2ed70d91f373a02b63abc908e9d81bf
Rob Brackett <rob@robbrackett.com>
2022-04-17 16:50:28 -0700

    Delete deprecated `appointment-*` docker repos
    These repositories have been out of use for almost a year, and are overdue for actual cleanup and deletion.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,33 +1,3 @@
-# DEPRECATED: Images should now all be in univaf-server (see below).
-resource "aws_ecr_repository" "server" {
-  name                 = "appointment-server"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-# DEPRECATED: Images should now all be in univaf-db-seed (see below).
-resource "aws_ecr_repository" "seed" {
-  name                 = "appointment-db-seed"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-# DEPRECATED: Images should now all be in univaf-loader (see below).
-resource "aws_ecr_repository" "loader" {
-  name                 = "appointment-loader"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
 resource "aws_ecr_repository" "server_repository" {
   name                 = "univaf-server"
   image_tag_mutability = "IMMUTABLE"
