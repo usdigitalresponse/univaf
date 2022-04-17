8714d0a0f7d9a2f5e087a038555455dd28154281
Rob Brackett <rob@robbrackett.com>
2022-04-17 16:58:44 -0700

    Make Docker image tags immutable in ECR (#636)
    Also removes old, deprecated container registries that have been out-of-use for a long time.
    
    Fixes #364.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,36 +1,6 @@
-# DEPRECATED: Images should now all be in univaf-server (see below).
-resource "aws_ecr_repository" "server" {
-  name                 = "appointment-server"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-# DEPRECATED: Images should now all be in univaf-db-seed (see below).
-resource "aws_ecr_repository" "seed" {
-  name                 = "appointment-db-seed"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-# DEPRECATED: Images should now all be in univaf-loader (see below).
-resource "aws_ecr_repository" "loader" {
-  name                 = "appointment-loader"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
 resource "aws_ecr_repository" "server_repository" {
   name                 = "univaf-server"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
@@ -39,7 +9,7 @@ resource "aws_ecr_repository" "server_repository" {
 
 resource "aws_ecr_repository" "seed_repository" {
   name                 = "univaf-db-seed"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
@@ -48,7 +18,7 @@ resource "aws_ecr_repository" "seed_repository" {
 
 resource "aws_ecr_repository" "loader_repository" {
   name                 = "univaf-loader"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
