6bdc88545f24894ae5aa76cd9dd770e3d010657b
Rob Brackett <rob@robbrackett.com>
2022-06-24 09:32:02 -0700

    Add manual corrections to NJVSS (#735)
    This follows the pattern of corrections we added for Albertsons, and solves issues with a few bad addresses.

---
+++ loader/src/sources/njvss/corrections.js
@@ -0,0 +1,19 @@
+/**
+ * Manual corrections for misformatted/bad source data. The keys are the
+ * `res_id` property for the location, and the value is any properties that
+ * should be overridden.
+ */
+module.exports.corrections = {
+  "d591d023-b457-eb11-a812-001dd8018866": {
+    vras_provideraddress: "187 North Ave, Dunellen, NJ 08812",
+  },
+  "c96a8022-b457-eb11-a812-001dd8020dc1": {
+    vras_provideraddress: "174 Passaic St, Garfield, NJ 07026",
+  },
+  "1498cb6f-8c91-eb11-a812-001dd802f301": {
+    vras_provideraddress: "307 1st Street, Hoboken, NJ 07030",
+  },
+  "b951acec-32f1-eb11-bacb-001dd80283f5": {
+    vras_provideraddress: "1400 Parkway Ave # A2, Ewing, NJ 08628",
+  },
+};
