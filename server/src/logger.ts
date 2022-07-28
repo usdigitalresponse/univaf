d836f17bebd0b35d2908f03ce67fb9dad804aba7
Rob Brackett <rob@robbrackett.com>
2022-07-28 12:51:08 -0700

    Simplify logs on Render

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -2,17 +2,27 @@ import { createLogger, Logger, transports, format } from "winston";
 const { combine, timestamp, splat, printf, label } = format;
 import { LOG_LEVEL } from "./config";
 
-const timestampPrefixLogFormat = printf(
-  ({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level}: ${message}`;
-  }
+// Some platforms include the timestamp for each log line, so adding our own
+// timestamp makes them harder to read.
+const shouldLogTimestamp = !(
+  process.env.RENDER ||
+  process.env.ECS_CONTAINER_METADATA_URI ||
+  process.env.ECS_CONTAINER_METADATA_URI_V4
 );
+
+const univafLogFormat = printf(({ level, message, label, timestamp }) => {
+  let formatted = `[${label}] ${level}: ${message}`;
+  if (shouldLogTimestamp) {
+    formatted = `${timestamp} ${formatted}`;
+  }
+  return formatted;
+});
 
 const customFormatWithTimestamp = combine(
   label({ label: "univaf" }),
   timestamp(),
   splat(),
-  timestampPrefixLogFormat
+  univafLogFormat
 );
 
 export const logger: Logger = createLogger({
