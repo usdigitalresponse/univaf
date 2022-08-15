b395c7358dfa11749ade6ac074466f78abaef6ba
Rob Brackett <rob@robbrackett.com>
2022-08-15 13:40:11 -0700

    Graceful shutdown on more signals
    We used to shut down the server gracefully on SIGINT, but not SIGTERM. However, SIGTERM is what shuts us down in production, which is arguably the time at which we need to be more graceful! This sets up graceful shutdown for several relevant signals.
    
    It also makes sure we destroy availability logging connections during shutdown (not just normal data connections). This isn't a *huge* deal, since these connections would get reaped pretty quickly when the process dies anyway, but this should make things a little nicer on the DB.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,29 +2,44 @@ import app from "./app";
 import process from "process";
 import { db } from "./db";
 import { logger, logStackTrace } from "./logger";
+import { availabilityDb } from "./availability-log";
 
 const port = app.get("port");
 const env = app.get("env");
 
 /**
- * Kill on SIGINT
+ * Shutdown gracefully on SIGINT/SIGTERM/etc.
  */
+function handleSignal(signal = "signal") {
+  // Unhook handler so a second signal will immediately kill the process.
+  process.removeAllListeners(signal);
 
-process.on("SIGINT", () => {
-  logger.info("Received SIGINT: process exiting...");
-  db.destroy()
-    .then(() => process.exit(0))
-    .catch((error: Error) => {
-      logStackTrace(logger, error);
-      process.exit(1);
-    });
-});
+  function hardShutdown(error: Error) {
+    logStackTrace(logger, error);
+    process.exit(1);
+  }
+
+  logger.info(`Received ${signal}: process exiting...`);
+  server.close((error: Error) => {
+    if (error) {
+      hardShutdown(error);
+    }
+
+    Promise.all([db.destroy(), availabilityDb.destroy()])
+      .then(() => process.kill(process.pid, signal))
+      .catch(hardShutdown);
+  });
+}
+
+process.on("SIGINT", handleSignal);
+process.on("SIGTERM", handleSignal);
+process.on("SIGBREAK", handleSignal);
 
 /**
  * Start Express server.
  */
 const server = app.listen(port, () => {
-  logger.info(`App is running at http://localhost:${port} in ${env} mode`);
+  logger.info(`Server is running at http://localhost:${port} in ${env} mode`);
   logger.info("Press CTRL-C to stop\n");
 });
 
