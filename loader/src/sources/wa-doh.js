usdigitalresponse/univaf

commit d403d2f838773890e0171782f63179ec17ac866b

Check for and handle errors in HyVee responses (#834)

loader/src/sources/wa-doh.js
@@ -1,13 +1,14 @@
 // Washington State DoH hosts data for multiple states for some providers where
 // they have API access. (In practice, this is pretty much only Costco.)
 
+const assert = require("node:assert/strict");
 const { Available, LocationType } = require("../model");
 const {
   httpClient,
   matchVaccineProduct,
   createWarningLogger,
 } = require("../utils");
-const { HttpApiError } = require("../exceptions");
+const { assertValidGraphQl } = require("../exceptions");
 const allStates = require("../states.json");
 
 const warn = createWarningLogger("waDoh");
@@ -79,17 +80,6 @@ const INVALID_LOCATIONS = [
   "prep-mod-3696ec0d-3869-4ff4-88ce-1030db1639ef",
 ];
 
-class WaDohApiError extends HttpApiError {
-  parse(response) {
-    if (typeof response.body === "object") {
-      this.details = response.body;
-    } else {
-      this.details = JSON.parse(response.body);
-    }
-    this.message = this.details.errors.map((item) => item.message).join(", ");
-  }
-}
-
 /**
  *
  * @param {string} state 2-letter state abbreviation or state name to query
@@ -115,14 +105,16 @@ async function* queryState(state) {
         },
       },
     });
-    if (response.statusCode >= 400 || response.body.errors) {
-      throw new WaDohApiError(response);
-    }
+    assertValidGraphQl(response);
 
-    const data = response.body.data;
-    yield data.searchLocations.locations;
+    const data = response.body?.data?.searchLocations;
+    assert.ok(
+      Array.isArray(data?.locations) && data?.paging?.total,
+      `Response did not match expected format: ${JSON.stringify(response.body)}`
+    );
+    yield data.locations;
 
-    if (data.searchLocations.paging.total <= pageNum * pageSize) break;
+    if (data.paging.total <= pageNum * pageSize) break;
 
     pageNum++;
   }
@@ -343,5 +335,4 @@ module.exports = {
   API_URL,
   checkAvailability,
   formatLocation,
-  WaDohApiError,
 };

loader/src/exceptions.js
@@ -28,9 +28,36 @@ class HttpApiError extends Error {
   }
 }
 
+/**
+ * Represents errors received from a GraphQL query result.
+ */
+class GraphQlError extends HttpApiError {
+  parse(response) {
+    if (typeof response.body === "object") {
+      this.details = response.body;
+    } else {
+      this.details = JSON.parse(response.body);
+    }
+    this.message = this.details.errors.map((item) => item.message).join(", ");
+  }
+}
+
+/**
+ * If the HTTP response represents a GraphQL error, throws an instance of
+ * `GraphQlError` with relevant information.
+ * @param {http.IncomingMessage} response
+ */
+function assertValidGraphQl(response) {
+  if (response.statusCode >= 400 || response.body?.errors) {
+    throw new GraphQlError(response);
+  }
+}
+
 class ParseError extends Error {}
 
 module.exports = {
+  GraphQlError,
   HttpApiError,
   ParseError,
+  assertValidGraphQl,
 };

loader/src/sources/hyvee/index.js
@@ -14,9 +14,11 @@
  * not worthwhile at the moment.
  */
 
+const assert = require("node:assert/strict");
 const Sentry = require("@sentry/node");
 const { httpClient, createWarningLogger } = require("../../utils");
 const { LocationType, Available, VaccineProduct } = require("../../model");
+const { assertValidGraphQl } = require("../../exceptions");
 
 const API_URL = "https://www.hy-vee.com/my-pharmacy/api/graphql";
 const BOOKING_URL = "https://www.hy-vee.com/my-pharmacy/covid-vaccine-consent";
@@ -99,7 +101,14 @@ async function fetchRawData() {
     },
   });
 
-  return response.body.data.searchPharmaciesNearPoint;
+  assertValidGraphQl(response);
+
+  const result = response.body?.data?.searchPharmaciesNearPoint;
+  assert.ok(
+    Array.isArray(result),
+    `Response did not match expected format: ${JSON.stringify(response.body)}`
+  );
+  return result;
 }
 
 function formatLocation(data, checkedAt) {

loader/test/wa-doh.test.js
@@ -1,9 +1,9 @@
 const nock = require("nock");
+const { GraphQlError } = require("../src/exceptions");
 const { VaccineProduct } = require("../src/model");
 const {
   API_URL,
   checkAvailability,
-  WaDohApiError,
   formatLocation,
 } = require("../src/sources/wa-doh");
 const { expectDatetimeString, splitHostAndPath } = require("./support");
@@ -90,7 +90,7 @@ describe("Washington DoH API", () => {
     expect(result).toContainItemsMatchingSchema(locationSchema);
   });
 
-  it("should throw WaDohApiError with detailed error info", async () => {
+  it("should throw GraphQlError with detailed error info", async () => {
     nock(API_URL_BASE)
       .post(API_URL_PATH)
       .reply(500, {
@@ -109,7 +109,7 @@ describe("Washington DoH API", () => {
       () => null,
       (error) => error
     );
-    expect(error).toBeInstanceOf(WaDohApiError);
+    expect(error).toBeInstanceOf(GraphQlError);
     expect(error.message).toContain("Expected type Int!");
   });
 

commit 027a28d407ac0ce372d608dc7e92f91a615a2bc7

HOTFIX: handle empty responses from WA DOH
This fixes a bug introduced in #834.

loader/src/sources/wa-doh.js
@@ -109,7 +109,7 @@ async function* queryState(state) {
 
     const data = response.body?.data?.searchLocations;
     assert.ok(
-      Array.isArray(data?.locations) && data?.paging?.total,
+      Array.isArray(data?.locations) && typeof data?.paging?.total === "number",
       `Response did not match expected format: ${JSON.stringify(response.body)}`
     );
     yield data.locations;