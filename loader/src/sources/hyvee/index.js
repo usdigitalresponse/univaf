usdigitalresponse/univaf

commit 6d863c7ca5839d1f361c1fdb59d323899b3eb7d6

Handle Moderna ages 12-17 listings from HyVee (#787)
HyVee started listing "Moderna (12-17)" as a vaccine type, but it's not actually a different product from what's used for ages 12+ or 18+. The location in question also lists some younger pediatric vaccines, so presumably this doesn't have anything to do with the minimum age the pharmacist is qualified for, and presumably there are just some appointment slots that are age limited this way. We've classified these as normal `moderna` here, and that is probably a close enough approximation to make sense, rather than creating a new vaccine type.

loader/src/sources/hyvee/index.js
@@ -24,6 +24,9 @@ const SOURCE_NAME = "univaf-hyvee";
 const PROVIDER_NAME = "hyvee";
 const VACCINE_NAMES = {
   Moderna: VaccineProduct.moderna,
+  // Moderna 12-17 is the same product as 12+ or 18+. This is a little messy,
+  // since technically these might be separate appointment slots for teens.
+  "Moderna (12-17)": VaccineProduct.moderna,
   "Pediatric-Moderna (3-5)": VaccineProduct.modernaAge0_5,
   "Pfizer-BioNTech": VaccineProduct.pfizer,
   "Pediatric-Pfizer (5-11)": VaccineProduct.pfizerAge5_11,

commit c90e34602e30bd545af488b77875d96ac59c5349

Support Novavax at Hy-Vee stores (#837)
This also fixes a minor bug caused by the changes in #787. I should have updated the test fixtures then, which would have revealed the issue (duplicate entries in the product list).

Fixes https://sentry.io/organizations/usdr/issues/3512631864.

loader/src/sources/hyvee/index.js
@@ -34,6 +34,7 @@ const VACCINE_NAMES = {
   "Pediatric-Pfizer (5-11)": VaccineProduct.pfizerAge5_11,
   "Pediatric-Pfizer (under 5)": VaccineProduct.pfizerAge0_4,
   Janssen: VaccineProduct.janssen,
+  Novavax: VaccineProduct.novavax,
 };
 
 const warn = createWarningLogger("hyvee");
@@ -176,7 +177,7 @@ function formatProducts(vaccineData) {
       return result;
     });
 
-  return products.length ? products : undefined;
+  return products.length ? [...new Set(products)] : undefined;
 }
 
 async function getData(states) {