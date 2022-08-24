d81ac46ef519bef4d85737a6394c5554c787c11f
Rob Brackett <rob@robbrackett.com>
2022-08-24 08:52:13 -0700

    Run `terraform fmt`

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -9,17 +9,17 @@ resource "aws_security_group" "bastion_security_group" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = ""
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = ""
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
