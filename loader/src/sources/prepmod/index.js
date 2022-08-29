72dfdaa2f97e90a8530984e7da0df53c7af149be
Rob Brackett <rob@robbrackett.com>
2022-08-29 09:28:52 -0700

    Clean up PrepMod addresses during formatting (#843)
    Sometimes PrepMod data has full addresses in the `address.line` field, despite also having the city/state/etc. fields filled in. This attempts to clean that up automatically where possible, and also address issues where multiple lines are conjoined into one in the data, e.g. `Loussac Library / 3600 Denali St`.
    
    Fixes #458.

--- loader/src/sources/prepmod/index.js
+++ loader/src/sources/prepmod/index.js
@@ -6,6 +6,7 @@
  * contact at least one (and sometimes several) hosts in each state.
  */
 
+const { isDeepStrictEqual } = require("node:util");
 const { ApiClient } = require("../../api-client");
 const {
   EXTENSIONS,
@@ -24,6 +25,11 @@ const {
   createWarningLogger,
   DEFAULT_STATES,
 } = require("../../utils");
+
+/**
+ * Import types
+ * @typedef {import("../../smart-scheduling-links").SmartSchdulingLinksAddress} SmartSchdulingLinksAddress
+ */
 
 const API_PATH = "/api/smart-scheduling-links/$bulk-publish";
 
@@ -114,11 +120,7 @@ function formatLocation(host, validTime, locationInfo) {
     provider: "prepmod",
     location_type: LocationType.clinic,
 
-    address_lines: smartLocation.address.line,
-    city: smartLocation.address.city,
-    state: smartLocation.address.state,
-    postal_code: smartLocation.address.postalCode,
-    county: smartLocation.address.district || undefined,
+    ...formatAddress(smartLocation.address),
     position,
 
     booking_url: formatLocationBookingUrl(host, smartLocation),
@@ -133,6 +135,75 @@ function formatLocation(host, validTime, locationInfo) {
       available,
       slots,
     },
+  };
+}
+
+// Attempts to match "<city>, <state abbreviation> <zip>" so it can be removed
+// from the address lines.
+const nonAddressLinePattern =
+  /(^\s*|,\s+)[A-Za-z\s]+,\s+[A-Z]{2}\s*,?\s+(\d{5}(-\d{4})?|USA)\s*$/;
+
+// A lot of locations seem to have multiple address lines squished into one,
+// often using ", " or " | " or " / " as a separator. (" - " is common, too,
+// but is often used legitimately, e.g. in some road names.)
+const maybeAddressLineBreaks = /\s*,\s+|\s+[|/]\s+/g;
+// These are things that are *definitey* line breaks, as opposed to the above,
+// which are more fuzzy and should be surfaced for human review.
+const addressLineBreaks = new RegExp(
+  [
+    // Actual line breaks
+    String.raw`\s*\n\s*`,
+    // Things that might be line breaks if followed by a suite or unit number.
+    String.raw`(?:${maybeAddressLineBreaks.source})(?=(?:suite|ste\.?|unit)\s+#?\d+)`,
+    // " - " is a delimiter if followed by a suite or unit number, too.
+    String.raw`\s+-\s+(?=(?:suite|ste\.?|unit)\s+#?\d+)`,
+  ].join("|"),
+  "ig"
+);
+
+/**
+ * Clean up and re-format the source data's address object. Sometimes city/state
+ * info is included in the lines, or multiple lines are collapsed together.
+ * This is a best-effort attempt to clean those up. Returns an object with the
+ * relevant UNIVAF-style address fields.
+ * @param {SmartSchdulingLinksAddress} rawAddress
+ * @returns {any}
+ */
+function formatAddress(rawAddress) {
+  // Fixes that are definitely correct.
+  const cleanLines = rawAddress.line
+    // Fix missing space between "Suite" and the suite number.
+    .map((line) => line.replace(/\b(suite|ste\.?|unit)(#?)(\d+)/i, "$1 $2$3"))
+    // Remove city, state, and zip if they are included in the lines.
+    .map((line) => line.replace(nonAddressLinePattern, ""))
+    // Split on things we are sure are line breaks.
+    .flatMap((line) => line.split(addressLineBreaks).map((x) => x.trim()));
+
+  // Fixes that we may want to review for correctness.
+  const extraCleanLines = cleanLines.flatMap((line) =>
+    line.split(maybeAddressLineBreaks).map((x) => x.trim())
+  );
+
+  // If we changed things above, log a warning so we know about it. This code
+  // is still a little speculative, so this will help us evaluate whether it's
+  // behaving correctly. Once we are more certain, this can be removed.
+  if (!isDeepStrictEqual(cleanLines, extraCleanLines)) {
+    warn(
+      `Poorly formatted address lines: ${JSON.stringify(rawAddress.line)}`,
+      {
+        original: JSON.stringify(rawAddress.line),
+        formatted: JSON.stringify(extraCleanLines),
+      },
+      true
+    );
+  }
+
+  return {
+    address_lines: extraCleanLines,
+    city: rawAddress.city,
+    state: rawAddress.state,
+    postal_code: rawAddress.postalCode,
+    county: rawAddress.district || undefined,
   };
 }
 
