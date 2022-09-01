06c4e68cee2278f0fe4c749cb63a3b40e74b84d7
Rob Brackett <rob@robbrackett.com>
2022-08-31 17:50:15 -0700

    Get rid of bogus `more` module
    I was only using this for testing.

--- common/src/index.js
+++ common/src/index.js
@@ -1,9 +1,7 @@
-const more = require("./more");
 const states = require("./states.json");
 const utils = require("./utils");
 
 module.exports = {
   utils,
-  more,
   states,
 };
--- common/src/more.js
+++
@@ -1,5 +0,0 @@
-const moreData = 5.39;
-
-module.exports = {
-  moreData,
-};
