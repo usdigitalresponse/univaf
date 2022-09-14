8f71e15221d282c97d4f3140b849b885546d67c7
Rob Brackett <rob@robbrackett.com>
2022-09-13 18:23:57 -0700

    Stop running the daily data snapshot task on AWS

--- terraform/api.tf
+++ terraform/api.tf
@@ -139,17 +139,21 @@ module "daily_data_snapshot_task" {
   }
 }
 
-module "daily_data_snapshot_schedule" {
-  source = "./modules/schedule"
+# NOTE: This schedule is commented out so that the snapshot task does not run,
+# but is quick to turn back on if we find we need to (e.g. something goes
+# horribly wrong with Render in production, and we need to switch back to AWS.)
+#
+# module "daily_data_snapshot_schedule" {
+#   source = "./modules/schedule"
 
-  name            = module.daily_data_snapshot_task.name
-  schedule        = "cron(0 5 * * ? *)"
-  role            = aws_iam_role.ecs_task_execution_role.arn
-  cluster_arn     = aws_ecs_cluster.main.arn
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.ecs_tasks.id]
-  task_arn        = module.daily_data_snapshot_task.arn
-}
+#   name            = module.daily_data_snapshot_task.name
+#   schedule        = "cron(0 5 * * ? *)"
+#   role            = aws_iam_role.ecs_task_execution_role.arn
+#   cluster_arn     = aws_ecs_cluster.main.arn
+#   subnets         = aws_subnet.public.*.id
+#   security_groups = [aws_security_group.ecs_tasks.id]
+#   task_arn        = module.daily_data_snapshot_task.arn
+# }
 
 resource "aws_cloudwatch_log_group" "data_snapshot_log_group" {
   name              = "/ecs/${module.daily_data_snapshot_task.name}"
