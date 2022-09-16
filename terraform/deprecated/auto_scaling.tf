e83afaf9070f2a423024dd9729580cfdf7ded1b8
Rob Brackett <rob@robbrackett.com>
2022-09-15 22:13:07 -0700

    Fix broken autoscaling configuration in Terraform
    This is pretty subtle! After turning off the loaders in AWS today (#941), I expected the API to scale down to its minimum, but it did not. At first I thought something must somehow still be sending it data or reaching it for queries, but no, it's actually because we had a lower bound instead of an upper bound set on the downscaling (clearly a copy-paste error). This fixes it and also updates the deprecated configurations so someone referencing them won't make the same mistake.

--- terraform/deprecated/auto_scaling.tf
+++ terraform/deprecated/auto_scaling.tf
@@ -21,7 +21,7 @@ resource "aws_appautoscaling_policy" "up" {
     metric_aggregation_type = "Maximum"
 
     step_adjustment {
-      metric_interval_lower_bound = 0
+      metric_interval_upper_bound = 0
       scaling_adjustment          = 1
     }
   }
