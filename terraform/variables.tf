12c619488f2a5f6f5ee328cff142fe470c0a5a6c
Rob Brackett <rob@robbrackett.com>
2022-09-28 08:33:17 -0700

    Delete AWS networks, IAM, security groups etc. (#985)
    Part of #683 / #779. This deletes pretty much all the resources we no longer need except for stored logs and container registries. The only remaining step of our platform transition after this is some light cleanup and refactoring to our remaining Terraform files.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,21 +3,6 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
-variable "ecs_task_execution_role_name" {
-  description = "ECS task execution role name"
-  default     = "myEcsTaskExecutionRole"
-}
-
-variable "az_count" {
-  description = "Number of AZs to cover in a given region"
-  default     = 2
-}
-
-variable "api_port" {
-  description = "Port exposed by the docker image to redirect traffic to"
-  default     = 3000
-}
-
 variable "ssl_certificate_arn" {
   description = "To enable HTTPS, the ARN of an SSL certificate created with ACM in us-east-1"
   default     = ""
@@ -25,16 +10,6 @@ variable "ssl_certificate_arn" {
 
 variable "domain_name" {
   description = "The domain name to use for HTTPS traffic"
-  default     = ""
-}
-
-variable "api_remote_domain_name_test" {
-  description = "The domain name for a service outside AWS to send traffic to"
-  default     = ""
-}
-
-variable "ssl_certificate_arn_render_test" {
-  description = "To enable HTTPS, the ARN of an SSL certificate created with ACM in us-east-1"
   default     = ""
 }
 
