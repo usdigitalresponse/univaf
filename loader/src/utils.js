c4e0029c954085244d0d9b2dcfbbc756248f2bcc
Rob Brackett <rob@robbrackett.com>
2022-10-02 13:31:44 -0700

    Fix unescaped tab characters in CVS JSON data (#990)
    CVS is outputting invalid JSON (there are unescaped tab characters, which are not allowed in proper JSON) in their SMART Scheduling Links API. This escapes any unescaped tabs, which should solve the issue for now on our side. (I've also notified their lead dev about the issue via FHIR chat.)
    
    Fixes https://sentry.io/organizations/usdr/issues/3638566125.

--- loader/src/utils.js
+++ loader/src/utils.js
@@ -485,6 +485,9 @@ module.exports = {
       .filter(Boolean)
       .map((line, index) => {
         try {
+          // Escape any unescaped tab characters. This fixes an issue with
+          // improperly encoded JSON from some sources.
+          line = line.replace(/\t/g, "\\t");
           return JSON.parse(line);
         } catch (error) {
           throw new SyntaxError(`Error parsing line ${index + 1}: ${line}`);
