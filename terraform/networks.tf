af9f59e5ef1112156f71575e4dfc6c02dac125c0
Rob Brackett <rob@robbrackett.com>
2022-10-11 15:50:28 -0700

    AWS again: name VPCs and networks
    Give names to our VPC and networks so we can see what's going on in the AWS console.

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -10,6 +10,10 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "172.17.0.0/16"
+
+  tags = {
+    Name = "univaf-vpc"
+  }
 }
 
 
@@ -21,6 +25,10 @@ resource "aws_subnet" "public" {
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "univaf-public-${count.index}"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -42,6 +50,10 @@ resource "aws_subnet" "private" {
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = aws_vpc.main.id
+
+  tags = {
+    Name = "univaf-private-${count.index}"
+  }
 }
 
 # Each subnet needs a NAT gateway with an elastic IP so services get internet
