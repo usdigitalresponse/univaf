dccc9682a9c5aa97a40273d5b40712b1fb51c7bc
Rob Brackett <rob@robbrackett.com>
2022-10-11 13:55:38 -0700

    AWS again: reinstate database
    Recreate the database on AWS. This uses smaller default instance and disk size than we used to have, and is in-line with what we were using on Render (which seemed to work just fine). Should save a little on costs.

---
+++ terraform/db.tf
@@ -0,0 +1,23 @@
+
+module "db" {
+  source = "./modules/rds"
+
+  name     = "univaf-db"
+  database = "univaf" # RDS does not allow hyphens
+  password = var.db_password
+  username = var.db_user
+
+  allocated_storage            = var.db_size
+  instance_class               = var.db_instance
+  engine                       = "postgres"
+  engine_version               = "14"
+  performance_insights_enabled = true
+
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = aws_subnet.private[*].id
+  ingress_allow_security_groups = compact([
+    # TODO: allow access from ECS once we've set it up again.
+    # aws_security_group.ecs_tasks.id,
+    aws_security_group.bastion_security_group.id
+  ])
+}
