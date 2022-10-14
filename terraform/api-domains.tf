0e2c5d3825ddd6f6f770ae21af640234e5d2db97
Rob Brackett <rob@robbrackett.com>
2022-10-14 11:06:51 -0700

    Rearrange API service terraform definitions
    Rearrange all the terraform stuff specifically about the API server into three files prefixed with `api`:
    - `api.tf`: The API server task, and snapshot task, service, and load balancer
    - `api-autoscaling.tf`: The autoscaling policies for the API service (maybe this and the service and the load balancer should be together?)
    - `api-domains.tf`: The domains and CDN/caching layer for the API service. Essentially tied to the API server, but strictly required for it to be running in ECS.

---
+++ terraform/api-domains.tf
@@ -0,0 +1,192 @@
+# Domains and CDN/Caching Layers
+#
+# The DNS zone (defined by the `domain_name` variable) should be manually
+# created in the AWS console, but all the records for the domain and subdomains
+# are managed here in code.
+#
+# The domains all point to CloudFront distributions for caching and DOS
+# protection, but the CloudFront distribution might use an external service
+# (e.g. Render.com) as its origin. If there's an external origin, it's defined
+# by the `api_remote_domain_name` variable.
+
+# Domains ---------------------------------------------------------------------
+
+data "aws_route53_zone" "domain_zone" {
+  count = var.domain_name != "" ? 1 : 0
+  name  = var.domain_name
+}
+
+# DNS record for the domain specified in the `domain_name` variable.
+resource "aws_route53_record" "api_domain_record" {
+  count = var.domain_name != "" ? 1 : 0
+
+  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.univaf_api[0].domain_name
+    zone_id                = aws_cloudfront_distribution.univaf_api[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+# The `www.` subdomain. It is an alias for the primary domain name.
+resource "aws_route53_record" "api_www_domain_record" {
+  count   = var.domain_name != "" ? 1 : 0
+  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
+  name    = "www"
+  type    = "CNAME"
+  records = [var.domain_name]
+  ttl     = 300
+}
+
+# The `render.` subdomain.
+# This specifically points to the deployment on Render and should be deleted
+# when we tear down that deployment.
+resource "aws_route53_record" "api_render_domain_record" {
+  count = (
+    var.domain_name != ""
+    && var.api_remote_domain_name != "" ? 1 : 0
+  )
+  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
+  name    = "render"
+  type    = "CNAME"
+  records = [var.domain_name]
+  ttl     = 300
+}
+
+# The `ecs.` subdomain.
+# This specifically points to the deployment on ECS (as opposed to Render).
+resource "aws_route53_record" "api_ecs_domain_record" {
+  count = var.domain_name != "" ? 1 : 0
+
+  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
+  name    = "ecs"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.univaf_api_ecs[0].domain_name
+    zone_id                = aws_cloudfront_distribution.univaf_api_ecs[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
+# CloudFront ------------------------------------------------------------------
+
+# Use CloudFront as a caching layer in front of the remote API server (Render
+# does not provide a built-in one). Enabled only if var.domain,
+# var.api_remote_domain and var.ssl_certificate_arn are provided.
+resource "aws_cloudfront_distribution" "univaf_api" {
+  count = (
+    var.domain_name != ""
+    && var.ssl_certificate_arn != ""
+    && var.api_remote_domain_name != "" ? 1 : 0
+  )
+  enabled     = true
+  price_class = "PriceClass_100" # North America
+  aliases = [
+    var.domain_name,
+    "www.${var.domain_name}",
+    "render.${var.domain_name}"
+  ]
+  http_version = "http2and3"
+
+  origin {
+    origin_id   = var.domain_name
+    domain_name = var.api_remote_domain_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_protocol_policy = var.api_remote_domain_name != "" ? "https-only" : "http-only"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = var.domain_name
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    max_ttl                = 3600
+
+    forwarded_values {
+      headers      = ["Host", "Origin"]
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.ssl_certificate_arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+# Use CloudFront as a caching layer in front of the API server that's running
+# in ECS. Enabled only if var.domain, and var.ssl_certificate_arn are provided.
+resource "aws_cloudfront_distribution" "univaf_api_ecs" {
+  count = (
+    var.domain_name != ""
+    && var.ssl_certificate_arn != "" ? 1 : 0
+  )
+  enabled     = true
+  price_class = "PriceClass_100" # North America
+  aliases = [
+    "ecs.${var.domain_name}"
+  ]
+  http_version = "http2and3"
+
+  origin {
+    origin_id   = "ecs.${var.domain_name}"
+    domain_name = aws_alb.main.dns_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "ecs.${var.domain_name}"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    max_ttl                = 3600
+
+    forwarded_values {
+      headers      = ["Host", "Origin"]
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.ssl_certificate_arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
--- terraform/api.tf
+++ terraform/api.tf
@@ -1,111 +1,62 @@
-# Add DNS (optional)
-data "aws_route53_zone" "domain_zone" {
-  count = var.domain_name != "" ? 1 : 0
-  name  = var.domain_name
-}
-
-resource "aws_route53_record" "api_domain_record" {
-  count = var.domain_name != "" ? 1 : 0
-
-  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.univaf_api[0].domain_name
-    zone_id                = aws_cloudfront_distribution.univaf_api[0].hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "api_www_domain_record" {
-  count   = var.domain_name != "" ? 1 : 0
-  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
-  name    = "www"
-  type    = "CNAME"
-  records = [var.domain_name]
-  ttl     = 300
-}
-
-# Specifically points to the deployment on Render
-resource "aws_route53_record" "api_render_domain_record" {
-  count = (
-    var.domain_name != ""
-    && var.api_remote_domain_name != "" ? 1 : 0
-  )
-  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
-  name    = "render"
-  type    = "CNAME"
-  records = [var.domain_name]
-  ttl     = 300
-}
-
-# Specifically points to the deployment on ECS
-resource "aws_route53_record" "api_ecs_domain_record" {
-  count = var.domain_name != "" ? 1 : 0
-
-  zone_id = data.aws_route53_zone.domain_zone[0].zone_id
-  name    = "ecs"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.univaf_api_ecs[0].domain_name
-    zone_id                = aws_cloudfront_distribution.univaf_api_ecs[0].hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-# Daily Data Snapshot ---------------------------------------------------------
-
-module "daily_data_snapshot_task" {
-  source = "./modules/task"
-
-  name    = "daily-data-snapshot"
-  image   = "${aws_ecr_repository.server_repository.repository_url}:${var.api_release_version}"
-  command = ["node", "scripts/availability_dump.js", "--write-to-s3", "--clear-log"]
-  role    = aws_iam_role.ecs_task_execution_role.arn
-
-  env_vars = {
-    DB_HOST                 = module.db.host
-    DB_NAME                 = module.db.db_name
-    DB_USERNAME             = var.db_user
-    DB_PASSWORD             = var.db_password
-    SENTRY_DSN              = var.api_sentry_dsn
-    DATA_SNAPSHOT_S3_BUCKET = var.data_snapshot_s3_bucket
-    AWS_ACCESS_KEY_ID       = var.data_snapshot_aws_key_id
-    AWS_SECRET_ACCESS_KEY   = var.data_snapshot_aws_secret_key
-    AWS_DEFAULT_REGION      = var.aws_region
-  }
-}
-
-module "daily_data_snapshot_schedule" {
-  source = "./modules/schedule"
-
-  name            = module.daily_data_snapshot_task.name
-  schedule        = "cron(0 1 * * ? *)"
-  role            = aws_iam_role.ecs_task_execution_role.arn
-  cluster_arn     = aws_ecs_cluster.main.arn
-  subnets         = aws_subnet.private.*.id
-  security_groups = [aws_security_group.ecs_tasks.id]
-  task_arn        = module.daily_data_snapshot_task.arn
-}
-
-resource "aws_cloudwatch_log_group" "data_snapshot_log_group" {
-  name              = "/ecs/${module.daily_data_snapshot_task.name}"
-  retention_in_days = 30
-
-  tags = {
-    Name = module.daily_data_snapshot_task.name
-  }
-}
-
-resource "aws_cloudwatch_log_stream" "data_snapshot_log_stream" {
-  name           = "${module.daily_data_snapshot_task.name}-log-stream"
-  log_group_name = aws_cloudwatch_log_group.data_snapshot_log_group.name
-}
+# API Service
+#
+# The API server (code in the `server` directory) runs as a service on ECS. It
+# serves the public API and receives updates from the loaders.
+#
+# The API also has a scheduled job that runs once a day to archive the state of
+# the database and log all the updates it received that day. That job is also
+# a task that runs on ECS (but just a task that runs to completion, not a
+# service that ECS keeps running).
 
 # API Service -----------------------------------------------------------------
 
+# The actual task that runs on ECS.
+module "api_task" {
+  source = "./modules/task"
+
+  name  = "api"
+  image = "${aws_ecr_repository.server_repository.repository_url}:${var.api_release_version}"
+  role  = aws_iam_role.ecs_task_execution_role.arn
+  # Only certain CPU/Memory combinations are allowed. See:
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-tasks-size
+  cpu    = var.api_cpu
+  memory = var.api_memory
+  port   = var.api_port
+
+  # Enable Datadog
+  datadog_enabled = true
+  datadog_api_key = var.datadog_api_key
+
+  env_vars = {
+    RELEASE                   = var.api_release_version
+    DB_HOST                   = module.db.host
+    DB_NAME                   = module.db.db_name
+    DB_USERNAME               = var.db_user
+    DB_PASSWORD               = var.db_password
+    API_KEYS                  = join(",", var.api_keys)
+    SENTRY_DSN                = var.api_sentry_dsn
+    SENTRY_TRACES_SAMPLE_RATE = format("%.2f", var.api_sentry_traces_sample_rate)
+    PRIMARY_HOST              = var.domain_name
+  }
+
+  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+}
+
+resource "aws_cloudwatch_log_group" "api_log_group" {
+  name              = "/ecs/api"
+  retention_in_days = 30
+
+  tags = {
+    Name = "api"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "api_log_stream" {
+  name           = "api-log-stream"
+  log_group_name = aws_cloudwatch_log_group.api_log_group.name
+}
+
+# The service's load balancer.
 resource "aws_alb" "main" {
   name                       = "api-load-balancer"
   subnets                    = aws_subnet.public.*.id
@@ -164,51 +115,9 @@ resource "aws_alb_listener_rule" "redirect_www" {
   }
 }
 
-module "api_task" {
-  source = "./modules/task"
-
-  name  = "api"
-  image = "${aws_ecr_repository.server_repository.repository_url}:${var.api_release_version}"
-  role  = aws_iam_role.ecs_task_execution_role.arn
-  # Only certain CPU/Memory combinations are allowed. See:
-  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-tasks-size
-  cpu    = var.api_cpu
-  memory = var.api_memory
-  port   = var.api_port
-
-  # Enable Datadog
-  datadog_enabled = true
-  datadog_api_key = var.datadog_api_key
-
-  env_vars = {
-    RELEASE                   = var.api_release_version
-    DB_HOST                   = module.db.host
-    DB_NAME                   = module.db.db_name
-    DB_USERNAME               = var.db_user
-    DB_PASSWORD               = var.db_password
-    API_KEYS                  = join(",", var.api_keys)
-    SENTRY_DSN                = var.api_sentry_dsn
-    SENTRY_TRACES_SAMPLE_RATE = format("%.2f", var.api_sentry_traces_sample_rate)
-    PRIMARY_HOST              = var.domain_name
-  }
-
-  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
-}
-
-resource "aws_cloudwatch_log_group" "api_log_group" {
-  name              = "/ecs/api"
-  retention_in_days = 30
-
-  tags = {
-    Name = "api"
-  }
-}
-
-resource "aws_cloudwatch_log_stream" "api_log_stream" {
-  name           = "api-log-stream"
-  log_group_name = aws_cloudwatch_log_group.api_log_group.name
-}
-
+# This service definition keeps the API server task running, connects it to the
+# load balancer, and manages multiple instances. (The actual scaling policies
+# are in a separate file.)
 resource "aws_ecs_service" "api_service" {
   name            = "api"
   cluster         = aws_ecs_cluster.main.id
@@ -237,118 +146,54 @@ resource "aws_ecs_service" "api_service" {
   depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role, module.api_task]
 }
 
-# Use CloudFront as a caching layer in front of the API server (Render does not
-# provide a built-in one). Enabled only if var.domain, var.api_remote_domain and
-# var.ssl_certificate_arn are provided.
-resource "aws_cloudfront_distribution" "univaf_api" {
-  count = (
-    var.domain_name != ""
-    && var.ssl_certificate_arn != ""
-    && var.api_remote_domain_name != "" ? 1 : 0
-  )
-  enabled     = true
-  price_class = "PriceClass_100" # North America
-  aliases = [
-    var.domain_name,
-    "www.${var.domain_name}",
-    "render.${var.domain_name}"
-  ]
-  http_version = "http2and3"
 
-  origin {
-    origin_id   = var.domain_name
-    domain_name = var.api_remote_domain_name
+# Daily Data Snapshot ---------------------------------------------------------
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
-      origin_protocol_policy = var.api_remote_domain_name != "" ? "https-only" : "http-only"
-    }
-  }
+# The snapshot task runs once a day to archive the contents of the database and
+# update logs to S3 so others can do historical analysis.
+module "daily_data_snapshot_task" {
+  source = "./modules/task"
 
-  default_cache_behavior {
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = var.domain_name
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    max_ttl                = 3600
+  name    = "daily-data-snapshot"
+  image   = "${aws_ecr_repository.server_repository.repository_url}:${var.api_release_version}"
+  command = ["node", "scripts/availability_dump.js", "--write-to-s3", "--clear-log"]
+  role    = aws_iam_role.ecs_task_execution_role.arn
 
-    forwarded_values {
-      headers      = ["Host", "Origin"]
-      query_string = true
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = var.ssl_certificate_arn
-    ssl_support_method  = "sni-only"
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
+  env_vars = {
+    DB_HOST                 = module.db.host
+    DB_NAME                 = module.db.db_name
+    DB_USERNAME             = var.db_user
+    DB_PASSWORD             = var.db_password
+    SENTRY_DSN              = var.api_sentry_dsn
+    DATA_SNAPSHOT_S3_BUCKET = var.data_snapshot_s3_bucket
+    AWS_ACCESS_KEY_ID       = var.data_snapshot_aws_key_id
+    AWS_SECRET_ACCESS_KEY   = var.data_snapshot_aws_secret_key
+    AWS_DEFAULT_REGION      = var.aws_region
   }
 }
 
-# Use CloudFront as a caching layer in front of the API server that's running
-# in ECS. Enabled only if var.domain, and var.ssl_certificate_arn are provided.
-resource "aws_cloudfront_distribution" "univaf_api_ecs" {
-  count = (
-    var.domain_name != ""
-    && var.ssl_certificate_arn != "" ? 1 : 0
-  )
-  enabled     = true
-  price_class = "PriceClass_100" # North America
-  aliases = [
-    "ecs.${var.domain_name}"
-  ]
-  http_version = "http2and3"
+module "daily_data_snapshot_schedule" {
+  source = "./modules/schedule"
 
-  origin {
-    origin_id   = "ecs.${var.domain_name}"
-    domain_name = aws_alb.main.dns_name
+  name            = module.daily_data_snapshot_task.name
+  schedule        = "cron(0 1 * * ? *)"
+  role            = aws_iam_role.ecs_task_execution_role.arn
+  cluster_arn     = aws_ecs_cluster.main.arn
+  subnets         = aws_subnet.private.*.id
+  security_groups = [aws_security_group.ecs_tasks.id]
+  task_arn        = module.daily_data_snapshot_task.arn
+}
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
-      origin_protocol_policy = "http-only"
-    }
+resource "aws_cloudwatch_log_group" "data_snapshot_log_group" {
+  name              = "/ecs/${module.daily_data_snapshot_task.name}"
+  retention_in_days = 30
+
+  tags = {
+    Name = module.daily_data_snapshot_task.name
   }
+}
 
-  default_cache_behavior {
-    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "ecs.${var.domain_name}"
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    max_ttl                = 3600
-
-    forwarded_values {
-      headers      = ["Host", "Origin"]
-      query_string = true
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = var.ssl_certificate_arn
-    ssl_support_method  = "sni-only"
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+resource "aws_cloudwatch_log_stream" "data_snapshot_log_stream" {
+  name           = "${module.daily_data_snapshot_task.name}-log-stream"
+  log_group_name = aws_cloudwatch_log_group.data_snapshot_log_group.name
 }
