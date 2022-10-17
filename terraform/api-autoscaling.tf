708399ec0350c2630e1299fb022e3d9b30a44605
Rob Brackett <rob@robbrackett.com>
2022-10-17 11:57:10 -0700

    Use more smaller API server instances (#1037)
    One lesson from rebuilding things on AWS is that we just don't use much memory in the API server. We can't reduce RAM without also cutting the CPU in half, but my working theory is that we can just double the instances and it should be fine. We'll follow the charts and see how this goes (honestly I think a baseline of more instances might make the whole thing more stable anyway).

--- terraform/api-autoscaling.tf
+++ terraform/api-autoscaling.tf
@@ -9,7 +9,7 @@ resource "aws_appautoscaling_target" "target" {
   service_namespace  = "ecs"
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.api_service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  min_capacity       = 1
+  min_capacity       = 2
   max_capacity       = 4
 }
 
