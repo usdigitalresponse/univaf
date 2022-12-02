aff332875a582ef46ada6286b5ad7592ce119acb
Rob Brackett <rob@robbrackett.com>
2022-12-02 13:21:48 -0800

    Oops, commit missing Jest config for common

---
+++ common/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  globals: {},
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  testMatch: ["**/test/**/*.test.(ts|js)", "!**/dist/test/**/*"],
+  testEnvironment: "node",
+  reporters: ["default", "github-actions"],
+  coverageReporters: ["text", "html"],
+};
