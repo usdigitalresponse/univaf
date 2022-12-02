b036863a5535c268a0bc4aed72f67d51fb641a6d
Rob Brackett <rob@robbrackett.com>
2022-12-02 15:12:10 -0800

    Move code formatting tools to top level

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,46 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+
   rules: {
-    "no-unused-vars": [
+    "prefer-const": ["error", { destructuring: "all" }],
+    // Will be part of eslint:recommended in ESlint 9, so we can remove then.
+    "no-new-native-nonconstructor": ["error"],
+
+    // TypeScript-focused rules -----------------------------------------------
+
+    // There is a TypeScript-enhanced version of no-unused-vars; use it instead
+    // of the built-in. (It will still check .js files, too.)
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
       "error",
       {
         argsIgnorePattern: "^_",
       },
     ],
-    "prefer-const": ["error", { destructuring: "all" }],
 
-    // Will be part of eslint:recommended in ESlint 9, so we can remove then.
-    "no-new-native-nonconstructor": ["error"],
+    "@typescript-eslint/explicit-module-boundary-types": [
+      "warn",
+      {
+        allowArgumentsExplicitlyTypedAsAny: true,
+      },
+    ],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // We have some plain JS in the codebase that we don't want a build step
+    // for; they need `require()` support.
+    "@typescript-eslint/no-var-requires": "off",
   },
+
+  ignorePatterns: ["node_modules", "dist", "coverage", "scratch.*"],
 
   overrides: [
     {
