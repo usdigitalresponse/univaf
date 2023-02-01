46ca240690c0bed6e681e65bb755923b14c083ff
Rob Brackett <rob@robbrackett.com>
2023-02-01 14:14:32 -0800

    Revoke public access to data snapshots bucket
    The data in the data snapshots S3 bucket is now available via CloudFront at `https://archives.getmyvax.org`, so we are ready to revoke public read access to the bucket. Fixes #1180.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,26 +7,9 @@ resource "aws_s3_bucket" "data_snapshots" {
   bucket = "univaf-data-snapshots"
 }
 
-# FIXME: Change acl to "private" once we confirm CloudFront is working.
 resource "aws_s3_bucket_acl" "data_snapshots_acl" {
   bucket = aws_s3_bucket.data_snapshots.id
-  acl    = "public-read"
-}
-
-# FIXME: Remove policy once we confirm CloudFront is working.
-resource "aws_s3_bucket_policy" "data_snapshots" {
-  bucket = aws_s3_bucket.data_snapshots.id
-  policy = jsonencode({
-    Version = "2008-10-17"
-    Id      = "Policy8542383977173"
-    Statement = [{
-      Sid       = "PublicReadAccess"
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.data_snapshots.arn}/*"
-    }]
-  })
+  acl    = "private"
 }
 
 # Alternative deployments that are being tested write to this bucket instead.
