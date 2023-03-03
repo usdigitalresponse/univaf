8ad4d47a6f51f2b20bbd9888534ef63be32ac5f5
Rob Brackett <rob@robbrackett.com>
2023-03-02 22:55:56 -0800

    Remove deprecated security groups (#1332)
    Step 4 of #1327 (final step!). This cleans up Terraform and removes the security groups we deprecated in earlier steps.

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,12 +3,13 @@
 # Security groups control inbound and outbound network connections to various
 # services (databases, ECS tasks, EC2 instances, load balancers, etc.).
 
-# ALB Security Group: Edit to restrict access to the API Server
+# Traffic to the API server's load balancer.
 resource "aws_security_group" "lb" {
   name        = "univaf-api-load-balancer-security-group"
   description = "Controls access to the API server load balancer"
   vpc_id      = aws_vpc.main.id
 
+  # HTTP
   ingress {
     protocol    = "tcp"
     from_port   = 80
@@ -16,33 +17,12 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTPS
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# FIXME: remove this when no longer in use!
-# (Transitioning to `aws_security_group.api_server_tasks`)
-resource "aws_security_group" "ecs_tasks" {
-  name        = "univaf-ecs-tasks-security-group"
-  description = "Allow inbound access only from the load balancer"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    protocol        = "tcp"
-    from_port       = var.api_port
-    to_port         = var.api_port
-    security_groups = [aws_security_group.lb.id]
   }
 
   egress {
