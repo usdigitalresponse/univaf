0d833e80125a87917cdd58b0c774bcd90ddcca75
Rob Brackett <rob@robbrackett.com>
2023-03-02 19:35:37 -0800

    Fix typo

--- terraform/security.tf
+++ terraform/security.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "api_server_tasks" {
   }
 }
 
-# Cron Job-like tasks on ECS may need to reach out to other servicers to load
+# Cron Job-like tasks on ECS may need to reach out to other services to load
 # things, but nothing else should be initiating communication with them.
 resource "aws_security_group" "cron_job_tasks" {
   name        = "univaf-cron-job-tasks-security-group"
