f9323e8fd841fb1b477b16e4aff2836d53e9da7e
Rob Brackett <rob@robbrackett.com>
2023-03-27 21:36:09 -0700

    Stop disabling Sentry during tests
    This used to be needed in order for all the tracing APIs we use to work right during tests, but now it's breaking them. It seems like subsequent `Sentry.init()` calls are causing fresh copies of the Sentry modules to be loaded, so they are no longer using the classes we monkey-patch. In *theory*, turning off Sentry sending during tests like this is the right thing to do, but it looks complicated to do and keep things working now.

--- server/test/support/globalSetup.ts
+++ server/test/support/globalSetup.ts
@@ -1,11 +1,8 @@
-import * as Sentry from "@sentry/node";
 import { knex } from "knex";
 import { loadDbConfig } from "../../src/config";
 import { clearDatabase } from "./database-core";
 
 module.exports = async () => {
-  Sentry.init({ enabled: false });
-
   const testDb = knex(loadDbConfig());
   await clearDatabase(testDb);
   await testDb.destroy();
