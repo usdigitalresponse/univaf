51daefec0c939e4f1792cd40af449f766393e574
Rob Brackett <rob@robbrackett.com>
2023-03-27 19:34:21 -0700

    Send more info when Rite Aid queries are too big
    In #1382, I fixed an issue that was causing us to see query results in the Rite Aid scraper that were too big because we were using the wrong radius when loading detailed results. Since landing that, however, we've seen this error happen several times on the first, non-detailed query! (Before, I checked the last 15 occurrences and they were *only* on the second, detailed query... hmmmm.)
    
    In all the cases I looked at, the number of known Rite Aid locations within the search radius was *way* fewer than 100, so I'm not sure what's going on. My best idea at this point is to add more telemetry, so that's what I've done here. When we send this error to Sentry, we'll include a list of all the results and their distances.

--- loader/src/sources/riteaid/scraper.js
+++ loader/src/sources/riteaid/scraper.js
@@ -90,10 +90,24 @@ async function queryZipCode(zip, radius = 100, stores = null) {
 
   // This internal API returns only up to N locations and has no pagination.
   // If we get the maximum number of results back, we're probably missing data.
-  assert.ok(
-    response.body.data.stores?.length < maximumResultCount,
-    `Query for zip code ${zip} had too many results`
-  );
+  if (response.body.data.stores?.length >= maximumResultCount) {
+    // Track the results! We get this error in situations where it doesn't seem
+    // possible given the zip, radius, and all known Rite Aids.
+    const resultSummary = response.body.data.stores.map((store) => ({
+      store: store.storeNumber,
+      zip: store.zipcode,
+      miles: store.milesFromCenter,
+    }));
+    const farthest = resultSummary
+      .slice()
+      .sort((a, b) => a.miles - b.miles)
+      .at(-1);
+    warn(
+      `Query for zip code ${zip} had too many results`,
+      { radius, farthest, results: resultSummary },
+      true
+    );
+  }
 
   return response.body;
 }
