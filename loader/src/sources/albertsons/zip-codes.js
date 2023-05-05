0f93134212bff3fd713600341751f5614cb9879d
Rob Brackett <rob@robbrackett.com>
2023-05-05 08:31:33 -0700

    Fill in zips for the rest of the east coast
    why not

--- loader/src/sources/albertsons/zip-codes.js
+++ loader/src/sources/albertsons/zip-codes.js
@@ -32,12 +32,156 @@ const zipCodesCoveringAlbertsons = {
       ],
     },
   ],
+  DC: [
+    {
+      radius: 15,
+      zips: [
+        "20570", // Washington, DC
+      ],
+    },
+  ],
+  DE: [
+    {
+      radius: 50,
+      zips: [
+        "19906", // Dover
+      ],
+    },
+  ],
+  IA: [
+    {
+      radius: 50,
+      zips: [
+        "52777", // Wheatland
+      ],
+    },
+  ],
+  IL: [
+    {
+      radius: 50,
+      zips: [
+        "60180", // Union
+        "60950", // Manteno
+        "61285", // Thomson
+        "61701", // Bloomington
+      ],
+    },
+  ],
+  IN: [
+    {
+      radius: 50,
+      zips: [
+        "46347", // Kouts
+      ],
+    },
+  ],
+  MA: [
+    {
+      radius: 50,
+      zips: [
+        "02345", // Manomet
+        "02456", // New Town
+        "01097", // Woronoco
+      ],
+    },
+  ],
+  MD: [
+    {
+      radius: 50,
+      zips: [
+        "21236", // Nottingham
+        "21801", // Salisbury
+        "20640", // Indian Head
+        // Not needed to cover known stores, but just to cover entire state.
+        // "21750", // Hancock
+      ],
+    },
+    // Not needed to cover known stores, but just to cover entire state.
+    // {
+    //   radius: 15,
+    //   zips: [
+    //     "21520", // Accident
+    //   ],
+    // },
+  ],
+  ME: [
+    {
+      radius: 50,
+      zips: [
+        "04082", // South Windham
+        "04952", // Morrill
+        "04415", // Brownville Junction
+        // Not needed to cover known stores, but just to cover entire state.
+        // "04970", // Rangeley
+        // "04686", // Wesley
+        // "04760", // Monticello
+        // "04739", // Eagle Lake
+      ],
+    },
+    // Not needed to cover known stores, but just to cover entire state.
+    // {
+    //   radius: 75,
+    //   zips: [
+    //     "04478", // Rockwood
+    //   ],
+    // },
+  ],
+  NH: [
+    {
+      radius: 50,
+      zips: [
+        "03303", // Concord
+        "03583", // Jefferson
+      ],
+    },
+  ],
   NJ: [
     {
       radius: 50,
       zips: [
         "08217", // Elwood
         "07067", // Colonia
+      ],
+    },
+  ],
+  NY: [
+    {
+      radius: 50,
+      zips: [
+        "10541", // Mahopac
+      ],
+    },
+  ],
+  PA: [
+    {
+      radius: 50,
+      zips: [
+        "19460", // Phoenixville
+      ],
+    },
+  ],
+  RI: [
+    {
+      radius: 30,
+      zips: [
+        "02921", // Cranston
+      ],
+    },
+  ],
+  VA: [
+    {
+      radius: 50,
+      zips: [
+        // Doesn't cover much of Virginia, but covers all the Albertsons!
+        "20119", // Catlett
+      ],
+    },
+  ],
+  VT: [
+    {
+      radius: 50,
+      zips: [
+        "05676", // Waterbury
       ],
     },
   ],
