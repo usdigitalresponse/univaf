73fe79a1d2b6ce627d3fc69e38f8cc17726c1a4e
Rob Brackett <rob@robbrackett.com>
2023-05-04 19:10:00 -0700

    Oops, also need to do that with all the brand rows

--- server/migrations/20230505012235_unpadded_albertsons_store_number.js
+++ server/migrations/20230505012235_unpadded_albertsons_store_number.js
@@ -1,18 +1,54 @@
+const brandKeys = [
+  "acme",
+  "albertsons_market",
+  "albertsons",
+  "amigos",
+  "carrs",
+  "haggen",
+  "jewelosco",
+  "luckys",
+  "market_street",
+  "pak_n_save",
+  "pavilions",
+  "randalls_pharmacy",
+  "randalls",
+  "safeway",
+  "sav_on",
+  "shaws",
+  "star_market",
+  "tom_thumb",
+  "united",
+  "vons",
+  "albertsons_corporate",
+  "community_clinic",
+];
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-  const zeroPaddedRows = await knex("external_ids")
+  const zeroPaddedParentRows = await knex("external_ids")
     .select("provider_location_id", "system", "value")
     .where("system", "albertsons_store_number")
     .where("value", "~", String.raw`:0\d+$`);
 
-  const unpaddedRows = zeroPaddedRows.map((r) => ({
+  const unpaddedParentRows = zeroPaddedParentRows.map((r) => ({
     ...r,
     value: r.value.replace(/:0+/, ":"),
   }));
 
+  const zeroPaddedBrandRows = await knex("external_ids")
+    .select("provider_location_id", "system", "value")
+    .whereIn("system", brandKeys)
+    .where("value", "~", String.raw`^0\d+$`);
+
+  const unpaddedBrandRows = zeroPaddedBrandRows.map((r) => ({
+    ...r,
+    value: r.value.replace(/^0+/, ":"),
+  }));
+
+  const unpaddedRows = [...unpaddedParentRows, ...unpaddedBrandRows];
   if (unpaddedRows.length) {
     return knex("external_ids")
       .insert(unpaddedRows)
