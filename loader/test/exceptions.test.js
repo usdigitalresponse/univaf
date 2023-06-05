20a69e2f3a2b27ada12dcd80f8967d611c021a12
Rob Brackett <rob@robbrackett.com>
2023-06-05 09:53:59 -0700

    Clean up duplicative GraphQL error messages
    GraphQL error responses are a list of N errors, and we often see the same error repeated multiple times (for example, this one is just the same "internal server error" message repeated 280 times: https://usdr.sentry.io/issues/4228325474/events/b3e7cf68182e4786b5a660d835880367/). To keep errors readable, our GraphQlError class now dedupes repeated messages.
    
    For example, instead of this error:
    
        GraphQlError: Internal Server Error, Internal Server Error, Internal Server Error...
    
    We'll now see:
    
        GraphQlError: Internal Server Error (×280)
    
    This also makes some minor improvements to the JSDoc types for GraphQlError.

---
+++ loader/test/exceptions.test.js
@@ -0,0 +1,34 @@
+const { GraphQlError } = require("../src/exceptions");
+
+describe("GraphQlError", () => {
+  it("Should format a nice message aggregating all the messages", () => {
+    const error = new GraphQlError({
+      statusCode: 400,
+      body: {
+        errors: [
+          { message: "First error", code: "ERROR__TYPE_1" },
+          { message: "Second error", code: "ERROR__TYPE_2" },
+        ],
+      },
+    });
+
+    expect(error.message).toContain("First error");
+    expect(error.message).toContain("Second error");
+  });
+
+  it("Should only include unique errors", () => {
+    const error = new GraphQlError({
+      statusCode: 400,
+      body: {
+        errors: [
+          { message: "First error", code: "ERROR__TYPE_1" },
+          { message: "Second error", code: "ERROR__TYPE_2" },
+          { message: "First error", code: "ERROR__TYPE_1" },
+        ],
+      },
+    });
+
+    const matchCount = [...error.message.matchAll(/First error/g)].length;
+    expect(matchCount).toBe(1);
+  });
+});
