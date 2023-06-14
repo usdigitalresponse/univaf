c2296baff317db01cf62c19a719c49bf6a0eb065
Rob Brackett <rob@robbrackett.com>
2023-06-14 11:54:31 -0700

    Add note about AWS Terraform provider version (#1568)
    We use a Terraform module that isn't yet compatible with v5 of the AWS provider, and this adds a note about the situation, since it might not be clear to someone coming in fresh to the codebase. Hopefully Cloudposse will update the module we use, but it doesn't look like that's coming soon, given that they recently updated their tutorials to specify AWS < 5. (See https://github.com/cloudposse/tutorials/pull/21#discussion_r1219948021)

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      # We use a module that is not yet compatible with v5 of the AWS provider:
+      # https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/issues/279
       version = "~> 4.52"
     }
   }
