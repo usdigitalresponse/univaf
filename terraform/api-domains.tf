ecb582c885a8c73d632ea1c3e237bc84ef881c0c
Rob Brackett <rob@robbrackett.com>
2023-06-16 10:45:03 -0700

    Revert "Replace old DNS record"
    This reverts commit df37a4cca7a8693a5d2939c88ac9b1ce937fd452.

--- terraform/api-domains.tf
+++ terraform/api-domains.tf
@@ -35,18 +35,13 @@ data "aws_route53_zone" "domain_zone" {
 }
 
 # DNS record for the domain specified in the `domain_name` variable.
-resource "aws_route53_record" "api_domain_record" {
-  count = var.domain_name != "" ? 1 : 0
-
+resource "aws_route53_record" "api_apex_domain_record" {
+  count   = var.domain_name != "" ? 1 : 0
   zone_id = data.aws_route53_zone.domain_zone[0].zone_id
   name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.univaf_api_ecs[0].domain_name
-    zone_id                = aws_cloudfront_distribution.univaf_api_ecs[0].hosted_zone_id
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  records = [var.domain_name_remote_api]
+  ttl     = 300
 }
 
 # The `www.` subdomain. It is an alias for the primary domain name.
